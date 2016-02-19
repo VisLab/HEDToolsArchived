0b3bc923821556ddd5771484ef55163eaab3a34b
jcockfie <jeremy.cockfield@gmail.com>
2016-02-19 10:48:09 -0600

    renamed test_checkrequired and fixed unit tests

--- validation/matlab/tests/test_checkRequiredTags.m
+++ validation/matlab/tests/test_checkRequiredTags.m
@@ -1,11 +1,11 @@
-function test_suite = test_checkrequired%#ok<STOUT>
+function test_suite = test_checkRequiredTags%#ok<STOUT>
 initTestSuite;
 
 function values = setup %#ok<DEFNU>
 % Read in the HED schema
-latestHed = 'HED 2.026.xml';
-values.Maps = parsehedxml(latestHed);
-requiredTags = values.Maps.required.values();
+load('HEDMaps.mat');
+values.hedMaps = hedMaps;
+requiredTags = values.hedMaps.required.values();
 values.Tags1 = {};
 values.Tags2 = requiredTags(1);
 values.Tags3 = requiredTags;
@@ -13,25 +13,24 @@ values.Tags3 = requiredTags;
 function teardown(values) %#ok<INUSD,DEFNU>
 % Function executed after each test
 
-
 function testRequiredTags(values)  %#ok<DEFNU>
 % Unit test for editmaps
 fprintf('\nUnit tests for checkrequired\n');
 
 fprintf(['\nIt should return errors when there are no required tags' ...
     ' present\n']);
-[errors, tags] = checkrequired(values.Maps, values.Tags1);
+[errors, tags] = checkRequiredTags(values.hedMaps, values.Tags1);
 assertFalse(isempty(errors));
-assertEqual(length(tags), length(values.Maps.required.values()));
-assertEqual(tags, values.Maps.required.values());
+assertEqual(length(tags), length(values.hedMaps.required.values()));
+assertEqual(tags, values.hedMaps.required.values());
 
 fprintf(['\nIt should return errors when there is one required tag' ...
     ' present and the others are missing\n']);
-[errors, tags] = checkrequired(values.Maps, values.Tags2);
+[errors, tags] = checkRequiredTags(values.hedMaps, values.Tags2);
 assertFalse(isempty(errors));
-assertEqual(length(tags), length(values.Maps.required.values())-1);
+assertEqual(length(tags), length(values.hedMaps.required.values())-1);
 
 fprintf('\nIt should return no errors when all required tags are present');
-[errors, tags] = checkrequired(values.Maps, values.Tags3);
+[errors, tags] = checkRequiredTags(values.hedMaps, values.Tags3);
 assertTrue(isempty(errors));
 assertEqual(length(tags), 0);
