8358c5f7547d4feedc5f96e9ecefec5dfbe25daa
jcockfie <jeremy.cockfield@gmail.com>
2016-06-03 12:39:34 -0500

    fixed examples, changed plugin version, updated plugin, added readme

--- matlab/tagging_examples.m
+++ matlab/tagging_examples.m
@@ -2,18 +2,13 @@
 % directory of EEG datasets, and a EEG study. 
 
 %% Set the example directory (PLEASE SET THIS)
-exampleDir = 'Path\To\HEDToolsTestArchive';
+exampleDir = 'HEDToolsExampleArchive';
 
 %% Tag the data in the EEG structure
 EEG = pop_loadset([exampleDir filesep ...
     'EEGLABSet' filesep 'eeglab_data_ch1.set']);
-[EEG, fMap, excluded] = tageeg(EEG); %#ok<*ASGLU>
+[EEG, fMap, excluded] = tageeg(EEG, 'SaveDataset', true); %#ok<*ASGLU>
 
 %% Tag the data in a specified directory
 [fMap, fPaths, excluded] = tagdir([exampleDir filesep 'EEGLABSet'], ...
-    'SaveDatasets', false);
-
-%% Tag the data represented by a study file
-[fMap, fPaths, excluded] = tagstudy([exampleDir filesep ... 
-    '5subjects' filesep 'n400clustedit.study'], ...
-    'SaveDatasets', false);
+    'SaveDatasets', true);
