19be3304e0220a1e2f5dd7d3d510e7f60300a868
jcockfie <jeremy.cockfield@gmail.com>
2016-06-20 17:08:45 -0500

    fixed gui layouts and fixed pop command strings

--- matlab/pop_validateeeg.m
+++ matlab/pop_validateeeg.m
@@ -1,14 +1,8 @@
 % pop_tageeg
-% Allows a user to tag a EEG structure using a GUI
+% Allows a user to validate a EEG structure using a GUI
 %
 % Usage:
-%   >>  [EEG, com] = pop_tageeg(EEG)
-%
-% [EEG, com] = pop_tageeg(EEG) takes an input EEGLAB EEG structure,
-% brings up a GUI to enter parameters for tageeg, and calls
-% tageeg to extracts the EEG structure's tags, if any.
-%
-% The tageeg function may optionally connect to a community tag database.
+%   >>  [errorLog, warningLog, extensionLog, com] = pop_validateeeg(EEG);
 %
 % Note: The primary purpose of pop_tageeg is to package up parameter input
 % and calling of tageeg for use as a plugin for EEGLAB (Edit menu).
@@ -17,11 +11,8 @@
 % See also:
 %   eeglab, tageeg, tagdir, tagstudy, and eegplugin_ctagger
 %
-
-%
 % Copyright (C) 2012-2013 Thomas Rognon tcrognon@gmail.com and
 % Kay Robbins, UTSA, kay.robbins@utsa.edu
-%
 %
 % This program is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -35,52 +26,46 @@
 %
 % You should have received a copy of the GNU General Public License
 % along with this program; if not, write to the Free Software
-% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-% $Log: pop_tageeg.m,v $
-% Revision 1.0 21-Apr-2013 09:25:25  kay
-% Initial version
-%
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 
 
 function [errorLog, warningLog, extensionLog, com] = ...
     pop_validateeeg(EEG)
-% Create the tagger for a single EEG file
+errorLog = '';
+warningLog = '';
+extensionLog = '';
 com = '';
 
-% Display help if inappropriate number of arguments
 if nargin < 1
-    help pop_tageeg;
+    help pop_validateeeg;
     return;
 end;
 
-% Get the tagger input parameters
 [cancelled, errorLogOnly, extensionsAllowed, hedXML, outDir] = ...
     validateeeg_input();
 if cancelled
     return;
 end
 
-% Tag the EEG structure and return the command string
 [errorLog, warningLog, extensionLog] = validateeeg(EEG, ...
     'errorLogOnly', errorLogOnly, ...
     'extensionAllowed', extensionsAllowed, ...
     'hedXML', hedXML, ...
     'outDir', outDir, ...
     'writeOutput', true);
-com = char(['validateeeg('' EEG '', ' ...
+com = char(['validateeeg(EEG, ' ...
     '''errorLogOnly'', ' logical2str(errorLogOnly) ', ' ...
     '''extensionAllowed'', ' logical2str(extensionsAllowed) ', ' ...
     '''hedXML'', ''' hedXML ''', ' ...
     '''outDir'', ''' outDir ''', ' ... 
-    '''writeOutput''' 'true)']);
-end % pop_tageeg
+    '''writeOutput'', ' 'true)']);
+end % pop_validateeeg
 
 function s = logical2str(b)
+% Converts a logical value to a string
 if b
     s = 'true';
 else
     s = 'false';
-end
 end % logical2str
-
+end % logical2str
