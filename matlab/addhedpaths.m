35582726714d04abc0c54bda46cfc614793801c0
Kay Robbins <Kay.Robbins@utsa.edu>
2016-11-21 07:24:32 -0600

    Added the saving of the base back in

--- matlab/addhedpaths.m
+++ matlab/addhedpaths.m
@@ -6,14 +6,24 @@ if isempty(tmp)
     myPath = fileparts(which('pop_tageeg.m'));
     addpath(genpath(myPath));
 end;
-tPath = fileparts(which('eegplugin_hedtools.m'));
 
-% Add the jar files needed to run this
-jarPath = [tPath filesep 'jars' filesep];  % With jar
+% Add the jar files needed to run this, preserving the base variables
 try
+    if ~exist(tempdir, 'dir')
+        warning('HEDTools: Your system does not have a temporary directory defined');
+        theDir = pwd; %#ok<*NASGU>
+    else
+        theDir = tempdir;
+    end
+    evalin('base', 'save([theDir ''tmp.mat''], ''-mat'');');
+    tPath = fileparts(which('eegplugin_hedtools.m'));
+    jarPath = [tPath filesep 'jars' filesep];  % With jar
+    
     javaaddpath([jarPath 'ctagger.jar']);
     javaaddpath([jarPath 'jackson.jar']);
     javaaddpath([jarPath 'hedconversion.jar']);
-catch mex   %#ok<NASGU>
+    evalin('base', 'load([theDir ''tmp.mat''], ''-mat'');');
+    delete([theDir 'tmp.mat']);    
+catch mex   
     warning('HEDTools: Could not add supporting Java tools to path');
 end
