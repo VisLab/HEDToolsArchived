8ca9d7c6450cdcbafcca733512ca2d5e299f33b3
jcockfie <jeremy.cockfield@gmail.com>
2017-04-11 08:56:43 -0500

    vector is sorted in vector2str before processed

--- matlab/helpers/utilities/vector2str.m
+++ matlab/helpers/utilities/vector2str.m
@@ -30,25 +30,22 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 function str = vector2str(num)
-str = '';
-if numel(num) == 1
-    str = num2str(num);
-elseif numel(num) > 1
-    str = num2str(num(1));
-    incrementStart = true;
-    for a = 2:length(num)
-        if num(a) - num(a-1) == 1
-            [str, incrementStart] = handleConsecutive(str, incrementStart);
-        else
-            [str, incrementStart] = handleNonConsecutive(str, ...
-                incrementStart, num(a-1), num(a));
-        end
+% Converts a vector to string with colon operators
+num = sort(num);
+str = num2str(num(1));
+incrementStart = true;
+for a = 2:length(num)
+    if num(a) - num(a-1) == 1
+        incrementStart = handleConsecutive(incrementStart);
+    else
+        incrementStart = handleNonConsecutive(num(a-1), num(a), ...
+            incrementStart);
     end
-    str = ['[' handleLastIndex(str, incrementStart, num(length(num))) ']'];
 end
+handleLastIndex(num(length(num)), incrementStart);
 
-    function [str, incrementStart] = handleNonConsecutive(str, ...
-            incrementStart, previous, current)
+    function incrementStart = handleNonConsecutive(previous, current, ...
+            incrementStart)
         % Handles a number that is not a consecutive number
         if ~incrementStart
             str = [str num2str(previous)];
@@ -57,14 +54,14 @@ end
         incrementStart = true;
     end % handleNonConsecutive
 
-    function str = handleLastIndex(str, incrementStart, last)
+    function handleLastIndex(last, incrementStart)
         % Handles the last number in the vecor
         if ~incrementStart
             str = [str num2str(last)];
         end
     end % handleLastIndex
 
-    function [str, incrementStart] = handleConsecutive(str, incrementStart)
+    function incrementStart = handleConsecutive(incrementStart)
         % Handles a number that is a consecutive number
         if incrementStart
             str = [str ':'];
