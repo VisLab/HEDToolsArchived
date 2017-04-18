5c71d7f9108fc347a8d8b3245151ccef9d82e574
jcockfie <jeremy.cockfield@gmail.com>
2017-04-18 09:58:22 -0500

    changed checkUniqueTags calls to checkunique in unit tests

--- matlab/tests/test_checkunique.m
+++ matlab/tests/test_checkunique.m
@@ -1,4 +1,4 @@
-function test_suite = test_checkUniqueTags%#ok<STOUT>
+function test_suite = test_checkunique%#ok<STOUT>
 initTestSuite;
 
 function values = setup %#ok<DEFNU>
@@ -21,20 +21,20 @@ function testRequiredTags(values)  %#ok<DEFNU>
 fprintf('\nUnit tests for checkunique\n');
 
 fprintf('\nIt should return no errors when there are no tags present');
-[errors, tags] = checkUniqueTags(values.Maps, values.Tags1, values.Tags1);
+[errors, tags] = checkunique(values.Maps, values.Tags1, values.Tags1);
 assertTrue(isempty(errors));
 assertTrue(isempty(tags));
 
 fprintf(['\nIt should return errors when there are multiple unique' ...
     ' tags with the same prefix present\n']);
-[errors, tags] = checkUniqueTags(values.Maps, values.Tags2, values.Tags2);
+[errors, tags] = checkunique(values.Maps, values.Tags2, values.Tags2);
 assertFalse(isempty(errors));
 assertFalse(isempty(tags));
 assertEqual(length(tags), 2);
 
 fprintf(['\nIt should return errors when there are multiple unique' ...
     ' tags in a tag group with the same prefix present\n']);
-[errors, tags] = checkUniqueTags(values.Maps, values.Tags3, values.Tags3);
+[errors, tags] = checkunique(values.Maps, values.Tags3, values.Tags3);
 assertFalse(isempty(errors));
 assertFalse(isempty(tags));
 assertEqual(length(tags), 2);
