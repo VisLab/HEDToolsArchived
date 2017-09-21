87c47237aab79a76e0355d43f1eb98f6c4e74fab
jcockfie <jeremy.cockfield@gmail.com>
2017-09-21 14:54:17 -0500

    added unit test for get_root_element function

--- python/tests/test_attribute_dictionary.py
+++ python/tests/test_attribute_dictionary.py
@@ -1,13 +1,23 @@
 import unittest;
-
+import defusedxml;
+from defusedxml.lxml import parse;
+import lxml;
 
 class Test(unittest.TestCase):
     @classmethod
     def setUpClass(self):
+        self.HED_XML = 'data/HED.xml';
         pass;
 
     @classmethod
     def tearDownClass(self):
         pass;
 
-    def test_parse_xml_file(self):
+    def test_get_root_element(self):
+        hed_tree = parse(self.HED_XML);
+        hed_root_element = hed_tree.getroot();
+        self.assertIsInstance(hed_tree, lxml.etree._ElementTree);
+        self.assertIsInstance(hed_root_element, defusedxml.lxml.RestrictedElement);
+
+if __name__ == '__main__':
+    unittest.main();
