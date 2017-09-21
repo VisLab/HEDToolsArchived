afebe10692062373d3be0d654c0e9d9a7be96e44
jcockfie <jeremy.cockfield@gmail.com>
2017-09-21 15:31:43 -0500

    implemented get_hed_root_element function

---
+++ python/validation/attribute_dictionary.py
@@ -0,0 +1,47 @@
+'''
+This module is used to store all HED tag attributes in a dictionary. The dictionary is a dictionary of dictionaries.
+Each dictionary pertains to a specific attribute associated with the HED tags.
+
+Created on Sept 21, 2017
+
+@author: Jeremy Cockfield
+
+'''
+
+from defusedxml.lxml import parse;
+
+
+extension_allowed_tags = {};
+
+def get_hed_root_element(hed_xml_file_path):
+    """Parses a xml file and returns the root element.
+
+    Parameters
+    ----------
+    hed_xml_file_path: string
+        The path to a HED XML file.
+
+    Returns
+    -------
+    RestrictedElement
+        The root element of the HED XML file.
+
+    """
+    hed_tree = parse(hed_xml_file_path);
+    return hed_tree.getroot();
+
+
+# def test_get_all_extension_allowed_tags():
+#     hed_tree = parse(self.HED_XML);
+#     hed_root_element = hed_tree.getroot();
+#     hed_node_elements = hed_root_element.findall('.//node');
+#     print(len(hed_node_elements))
+#     for hed_node_element in hed_node_elements:
+#         if 'extensionAllowed' in hed_node_element.attrib:
+#             print("Node name: " + hed_node_element.find('name').text);
+#             parent_node = hed_node_element.getparent();
+#             print("Parent Node name: " + parent_node.find('name').text)
+
+# def test_get_all_ancestor_tags(self):
+
+
