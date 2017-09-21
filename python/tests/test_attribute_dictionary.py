725eadc03fe379ca199d620238dae60f01fd31dd
jcockfie <jeremy.cockfield@gmail.com>
2017-09-21 11:16:20 -0500

    delete master branch files and added test and validation packages

---
+++ python/tests/test_attribute_dictionary.py
@@ -0,0 +1,13 @@
+import unittest;
+
+
+class Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        pass;
+
+    @classmethod
+    def tearDownClass(self):
+        pass;
+
+    def test_parse_xml_file(self):
