a811a3fa47e55859534ac59b16cbb4d4b09bab15
jcockfie <jeremy.cockfield@gmail.com>
2017-09-25 11:53:35 -0500

    added unit test for get_all_ancestor_tags function

--- python/tests/test_attribute_dictionary.py
+++ python/tests/test_attribute_dictionary.py
@@ -21,6 +21,22 @@ class Test(unittest.TestCase):
         self.assertIsInstance(parent_tag_name, basestring);
         self.assertTrue(parent_tag_name);
 
+    def test_get_parent_tag_name(self):
+        hed_root_element = attribute_dictionary.get_hed_root_element(self.HED_XML);
+        all_nodes = hed_root_element.xpath('.//node');
+        tag_element = all_nodes[24];
+        parent_tag_name = attribute_dictionary.get_parent_tag_name(tag_element);
+        self.assertIsInstance(parent_tag_name, basestring);
+        self.assertTrue(parent_tag_name);
+
+    def test_get_all_ancestor_tags(self):
+        hed_root_element = attribute_dictionary.get_hed_root_element(self.HED_XML);
+        all_nodes = hed_root_element.xpath('.//node');
+        tag_element = all_nodes[24];
+        all_ancestor_tags = attribute_dictionary.get_all_ancestor_tags(tag_element);
+        self.assertIsInstance(all_ancestor_tags, list);
+        self.assertTrue(all_ancestor_tags);
+
 
 if __name__ == '__main__':
     unittest.main();
