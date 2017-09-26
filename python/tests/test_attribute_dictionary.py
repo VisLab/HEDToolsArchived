62fb68846a5b4c8ef5df7667f407a9369a6b471b
jcockfie <jeremy.cockfield@gmail.com>
2017-09-26 13:20:34 -0500

    added unit test for string_list_2_lowercase_dictionary function

--- python/tests/test_attribute_dictionary.py
+++ python/tests/test_attribute_dictionary.py
@@ -11,6 +11,7 @@ class Test(unittest.TestCase):
         self.HED_XML = 'data/HED.xml';
         self.tag_attributes = ['extensionAllowed', 'requireChild', 'takesValue', 'isNumeric', 'required', \
                                'position', 'unique', 'predicateType'];
+        self.string_list = ['This/Is/A/Tag', 'This/Is/Another/Tag'];
 
     def test_get_hed_root_element(self):
         hed_root_element = attribute_dictionary.get_hed_root_element(self.HED_XML);
@@ -64,10 +65,14 @@ class Test(unittest.TestCase):
     def test_get_tag_paths_by_attribute(self):
         hed_root_element = attribute_dictionary.get_hed_root_element(self.HED_XML);
         for tag_attribute in self.tag_attributes:
-            print(tag_attribute);
             tag_paths = attribute_dictionary.get_tag_paths_by_attribute(hed_root_element, tag_attribute);
             self.assertIsInstance(tag_paths, list);
             self.assertTrue(tag_paths);
+
+    def test_string_list_2_lowercase_dictionary(self):
+        lowercase_dictionary = attribute_dictionary.string_list_2_lowercase_dictionary(self.string_list);
+        self.assertIsInstance(lowercase_dictionary, dict);
+        self.assertTrue(lowercase_dictionary);
 
 if __name__ == '__main__':
     unittest.main();
