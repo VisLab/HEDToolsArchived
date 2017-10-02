f02412b509afe8a24e44fd1ba5e7f10aac6fa614
jcockfie <jeremy.cockfield@gmail.com>
2017-10-02 13:06:53 -0500

    changed line error type message

--- python/validation/error_reporter.py
+++ python/validation/error_reporter.py
@@ -11,7 +11,7 @@ def report_error_type(error_type, error_line=1, tag='', tag_prefix='', unit_clas
         'isNumeric': '\tERROR: Invalid numeric tag - \"%s\"\n' % tag,
         'line': 'Issues on line %s:\n' % str(error_line),
         'required': '\tERROR: Tag with prefix \"%s\" is required\n' % tag,
-        'requireChild':'\tERROR: Descendant tag required - \"%s\"\n' % tag,
+        'requireChild':'tERROR: Descendant tag required - \"%s\"\n' % tag,
         'tilde': '\tERROR: Too many tildes - group \"%s\"\n' % tag,
         'unique': '\tERROR: Multiple unique tags (prefix \"%s\") - \"%s\"\n' % (tag_prefix, tag),
         'unitClass': '\tERROR: Invalid unit - \"%s\" (valid units are "%s")\n' % (tag, unit_class_units),
