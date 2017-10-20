b1189369025d46223a1c7c5e8179ba2133088c89
jcockfie <jeremy.cockfield@gmail.com>
2017-10-20 14:14:04 -0500

    updated function doc in tag_validator

--- python/validation/tag_validator.py
+++ python/validation/tag_validator.py
@@ -7,16 +7,18 @@ Created on Oct 2, 2017
 
 '''
 
+import random;
+
 from validation import error_reporter, tag_dictionary;
 
 def check_if_tag_is_valid(tag_dictionaries, original_tag, formatted_tag):
-    """Reports the validation error based on the type of error.
+    """Reports a validation error if the tag provided is not a valid tag or doesn't take a value.
 
     Parameters
     ----------
     tag_dictionaries: dictionary
         A dictionary containing containing all of the tags, tag attributes, unit class units, and unit class attributes.
-    original_tag: int
+    original_tag: string
         The original tag that is used to report the error.
     formatted_tag: string
         The tag that is used to do the validation.
@@ -32,6 +34,32 @@ def check_if_tag_is_valid(tag_dictionaries, original_tag, formatted_tag):
         validation_error = error_reporter.report_error_type(error_type, tag=original_tag);
     return validation_error;
 
+def check_if_tag_requires_child(tag_dictionaries, original_tag, formatted_tag):
+    """Reports a validation error if the tag provided has the 'requireChild' attribute.
+
+    Parameters
+    ----------
+    tag_dictionaries: dictionary
+        A dictionary containing containing all of the tags, tag attributes, unit class units, and unit class attributes.
+    original_tag: string
+        The original tag that is used to report the error.
+    formatted_tag: string
+        The tag that is used to do the validation.
+    Returns
+    -------
+    string
+        A validation error string. If no errors are found then an empty string is returned.
+
+    """
+    validation_error = '';
+    error_type = 'requireChild';
+    if tag_dictionaries['requireChild'].get(formatted_tag):
+        validation_error = error_reporter.report_error_type(error_type, tag=original_tag);
+    return validation_error;
+
 if __name__ == '__main__':
     tag_dictionaries = tag_dictionary.populate_tag_dictionaries('../tests/data/HED.xml');
-    print(check_if_tag_is_valid(tag_dictionaries, 'This/Is/A/Tag', 'this/is/a/tag'));
+    random_require_child_key = random.randint(0, len(tag_dictionaries['requireChild']));
+    print(tag_dictionaries.keys());
+    # required_child_tag = \
+    #     tag_dictionaries['requireChild']];
