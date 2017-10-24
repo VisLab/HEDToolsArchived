12ffc0eeec2a8c95ac4c45ad0b343457332fccb1
jcockfie <jeremy.cockfield@gmail.com>
2017-10-24 10:59:04 -0500

    added unit test for check_for_required_tags function

--- python/tests/test_tag_validator.py
+++ python/tests/test_tag_validator.py
@@ -18,13 +18,17 @@ class Test(unittest.TestCase):
         self.valid_formatted_tag = 'event/label';
         self.valid_tag_group_string = 'This/Is/A/Tag ~ This/Is/Another/Tag ~ This/Is/A/Different/Tag';
         self.invalid_tag_group_string = 'This/Is/A/Tag ~ ~ This/Is/Another/Tag ~ This/Is/A/Different/Tag';
-        self.valid_original_unique_tag_list = ['Event/Label/This is a label', \
+        self.valid_original_unique_tag_list = ['Event/Label/This is a label',
                                                'Event/Description/This is a description'];
-        self.valid_formatted_unique_tag_list = ['event/label/this is a label', \
+        self.valid_formatted_unique_tag_list = ['event/label/this is a label',
              'event/description/this is a description'];
         self.invalid_original_unique_tag_list = ['Event/Label/This is a label', 'Event/Label/This is another label',
              'Event/Description/This is a description'];
         self.invalid_formatted_unique_tag_list = ['event/label/this is a label', 'event/label/this is another label',
+             'event/description/this is a description'];
+        self.valid_formatted_required_tag_list = ['event/label/this is a label', 'event/category/participant response',
+             'event/description/this is a description'];
+        self.invalid_formatted_required_tag_list = ['event/label/this is a label',
              'event/description/this is a description'];
 
     def test_check_if_tag_is_valid(self):
@@ -66,6 +70,18 @@ class Test(unittest.TestCase):
                                                                              self.invalid_formatted_unique_tag_list);
         self.assertIsInstance(validation_error, basestring);
         self.assertTrue(validation_error);
+
+    def test_check_for_required_tags(self):
+        tag_dictionaries = tag_dictionary.populate_tag_dictionaries(self.hed_xml);
+        validation_error = tag_validator.check_for_required_tags(tag_dictionaries, \
+                                                                             self.valid_formatted_required_tag_list);
+        self.assertIsInstance(validation_error, basestring);
+        self.assertFalse(validation_error);
+        validation_error = tag_validator.check_for_required_tags(tag_dictionaries, \
+                                                                             self.invalid_formatted_required_tag_list);
+        self.assertIsInstance(validation_error, basestring);
+        self.assertTrue(validation_error);
+
 
 if __name__ == '__main__':
     unittest.main();
