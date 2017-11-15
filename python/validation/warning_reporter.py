d25b9745799e4532ec0d85a662829d4372f4ebef
jcockfie <jeremy.cockfield@gmail.com>
2017-11-15 09:20:34 -0600

    fixed unitClass warning

--- python/validation/warning_reporter.py
+++ python/validation/warning_reporter.py
@@ -8,7 +8,7 @@ Created on Oct 2, 2017
 '''
 
 
-def report_warning_type(warning_type, tag='', units=''):
+def report_warning_type(warning_type, tag='', default_unit=''):
     """Reports the validation warning based on the type of warning.
 
     Parameters
@@ -17,8 +17,8 @@ def report_warning_type(warning_type, tag='', units=''):
         The type of validation warning.
     tag: string
         The tag that generated the warning. The original tag not the formatted one.
-    units: string
-        The unit class units that are associated with the warning.
+    default_unit: string
+        The default unit class unit associated with the warning.
     Returns
     -------
     string
@@ -27,7 +27,7 @@ def report_warning_type(warning_type, tag='', units=''):
     """
     warning_types = {
         'cap': '\tWARNING: First word not capitalized or camel case - "%s"\n' % tag,
-        'unitClass': '\tWARNING: No unit specified. Using "%s" as the default - "%s"' % (units, tag)
+        'unitClass': '\tWARNING: No unit specified. Using "%s" as the default - "%s"' % (default_unit, tag)
     }
     return warning_types.get(warning_type, None);
 
