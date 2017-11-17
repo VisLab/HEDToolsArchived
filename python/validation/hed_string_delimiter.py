0d2f43568bcf672483a4743dcc140d5c6f13729a
jcockfie <jeremy.cockfield@gmail.com>
2017-11-17 14:36:58 -0600

    implemented get_tag_set function

--- python/validation/hed_string_delimiter.py
+++ python/validation/hed_string_delimiter.py
@@ -8,6 +8,9 @@ Created on Nov 15, 2017
 '''
 
 
+import copy;
+
+
 class HedStringDelimiter:
     DELIMITER = ',';
     OPENING_GROUP_CHARACTER = '(';
@@ -15,22 +18,33 @@ class HedStringDelimiter:
 
     def __init__(self, hed_string):
         self.hed_string = hed_string;
-        self.top_level_tag_set = self.split_top_level_hed_string_tags();
+        self.tag_set = self.split_hed_string();
 
-    def split_top_level_hed_string_tags(self):
-        """Splits the top-level tags and groups in a hed string based on a delimiter. The default delimiter is a comma.
+    def get_tag_set(self):
+        """Gets the tag_set field.
 
         Parameters
         ----------
-        hed_string: string
-            A hed string.
         Returns
         -------
         set
-            A set containing the individual tags and tag groups at the top-level of the hed string.
+            A set containing the individual tags and tag groups in the hed string. Nested tag groups are not split.
 
         """
-        top_level_tag_set = set();
+        return self.tag_set
+
+    def split_hed_string(self):
+        """Splits the tags and non-nested groups in a hed string based on a delimiter. The default delimiter is a comma.
+
+        Parameters
+        ----------
+        Returns
+        -------
+        set
+            A set containing the individual tags and tag groups in the hed string. Nested tag groups are not split.
+
+        """
+        tag_set = set();
         number_of_opening_parentheses = 0;
         number_of_closing_parentheses = 0;
         current_tag = '';
@@ -41,16 +55,54 @@ class HedStringDelimiter:
                 number_of_closing_parentheses += 1;
             if number_of_opening_parentheses == number_of_closing_parentheses and character == \
                     HedStringDelimiter.DELIMITER:
-                top_level_tag_set.add(current_tag.strip());
+                tag_set.add(current_tag.strip());
                 current_tag = '';
             else:
                 current_tag += character;
-        top_level_tag_set.add(current_tag.strip());
-        return top_level_tag_set;
+        tag_set.add(current_tag.strip());
+        return tag_set;
+
+    def get_top_level_tags(self):
+        """Gets the top-level tags from a hed string. All group tags will be removed.
+
+        Parameters
+        ----------
+        Returns
+        -------
+        set
+            A set containing the top level tags.
+
+        """
+        groups_to_remove = [];
+        event_level_tag_set = copy.deepcopy(self.tag_set);
+        for top_level_tag_or_group in event_level_tag_set:
+            if top_level_tag_or_group.startswith(HedStringDelimiter.OPENING_GROUP_CHARACTER) and \
+               top_level_tag_or_group.endswith(HedStringDelimiter.CLOSING_GROUP_CHARACTER):
+                groups_to_remove.append(top_level_tag_or_group);
+        event_level_tag_set = self.remove_elements_from_set(event_level_tag_set, groups_to_remove);
+        return event_level_tag_set;
+
+    def remove_elements_from_set(self, removal_set, removal_element_list):
+        """Remove a specified list elements from a set.
+
+        Parameters
+        ----------
+        removal_set: set
+            A set containing elements.
+        removal_element_list: list
+            A list of elements that will be removed from the set.
+        Returns
+        -------
+        set
+            A set with the removed elements.
+
+        """
+        for removal_element in removal_element_list:
+            removal_set.remove(removal_element);
+        return removal_set;
 
 if __name__ == '__main__':
     hed_string = 'tag1,(tag2,tag5,(tag1),tag6),tag2,(tag3,tag5,tag6),tag3';
     hed_string_delimiter = HedStringDelimiter(hed_string);
-    tags = hed_string_delimiter.split_top_level_hed_string_tags(hed_string);
-    for tag in tags:
-        print(tag);
+    tags = hed_string_delimiter.get_top_level_tags();
+    print(tags);
