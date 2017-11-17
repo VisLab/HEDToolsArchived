90aa9f969c730bc96b125da255d91ecb63715f46
jcockfie <jeremy.cockfield@gmail.com>
2017-11-17 15:03:04 -0600

    added unit test for remove_elements_from_set function

--- python/tests/test_hed_string_delimiter.py
+++ python/tests/test_hed_string_delimiter.py
@@ -10,9 +10,9 @@ class Test(unittest.TestCase):
 
     def test_split_hed_string(self):
         hed_string_delimiter = HedStringDelimiter(self.mixed_hed_string);
-        top_level_tag_set = hed_string_delimiter.split_hed_string();
-        self.assertTrue(top_level_tag_set);
-        self.assertIsInstance(top_level_tag_set, set);
+        tag_set = hed_string_delimiter._split_hed_string();
+        self.assertTrue(tag_set);
+        self.assertIsInstance(tag_set, set);
 
     def test_get_tag_set(self):
         hed_string_delimiter = HedStringDelimiter(self.mixed_hed_string);
@@ -20,13 +20,13 @@ class Test(unittest.TestCase):
         self.assertTrue(tag_set);
         self.assertIsInstance(tag_set, set);
 
-    # def test_remove_elements_from_set(self):
-    #     hed_string_delimiter = HedStringDelimiter(self.mixed_hed_string);
-    #     top_level_tags = hed_string_delimiter.split_hed_string();
-    #     set_with_removed_elements = hed_string_delimiter.remove_elements_from_set(self.mixed_hed_string,
-    #                                                                               self.removal_elements);
-    #     self.assertTrue(set_with_removed_elements);
-    #     self.assertIsInstance(set_with_removed_elements, set);
+    def test_remove_elements_from_set(self):
+        hed_string_delimiter = HedStringDelimiter(self.mixed_hed_string);
+        tag_set = hed_string_delimiter.get_tag_set();
+        tag_set_with_removed_elements = hed_string_delimiter.remove_elements_from_set(tag_set, self.removal_elements);
+        self.assertTrue(tag_set_with_removed_elements);
+        self.assertIsInstance(tag_set_with_removed_elements, set);
+        self.assertNotEqual(len(tag_set), len(tag_set_with_removed_elements));
 
 if __name__ == '__main__':
     unittest.main();
