27cd99375da5a27ccc42483de43079825e6e93dc
jcockfie <jeremy.cockfield@gmail.com>
2017-11-17 16:35:06 -0600

    added unit test for hed_string_is_a_group function

--- python/tests/test_hed_string_delimiter.py
+++ python/tests/test_hed_string_delimiter.py
@@ -6,6 +6,7 @@ class Test(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.mixed_hed_string = 'tag1,(tag2,tag5,(tag1),tag6),tag2,(tag3,tag5,tag6),tag3';
+        cls.group_hed_string = '(tag1, tag2)';
         cls.removal_elements = ['(tag2,tag5,(tag1),tag6)', '(tag3,tag5,tag6)'];
 
     def test_split_hed_string(self):
@@ -27,6 +28,14 @@ class Test(unittest.TestCase):
         self.assertTrue(top_level_tag_set);
         self.assertIsInstance(top_level_tag_set, set);
         self.assertNotEqual(len(tag_set), len(top_level_tag_set));
+
+    def test_hed_string_is_a_group(self):
+        is_group = HedStringDelimiter.hed_string_is_a_group(self.mixed_hed_string);
+        self.assertFalse(is_group);
+        self.assertIsInstance(is_group, bool);
+        is_group = HedStringDelimiter.hed_string_is_a_group(self.group_hed_string);
+        self.assertTrue(is_group);
+        self.assertIsInstance(is_group, bool);
 
 if __name__ == '__main__':
     unittest.main();
