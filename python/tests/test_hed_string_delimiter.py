650e83acf506ea09543336d42aca7ffb3ba648ab
jcockfie <jeremy.cockfield@gmail.com>
2017-11-17 14:36:15 -0600

    added unit test for get_tag_set

--- python/tests/test_hed_string_delimiter.py
+++ python/tests/test_hed_string_delimiter.py
@@ -6,12 +6,27 @@ class Test(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.mixed_hed_string = 'tag1,(tag2,tag5,(tag1),tag6),tag2,(tag3,tag5,tag6),tag3';
+        cls.removal_elements = ['(tag2,tag5,(tag1),tag6)', '(tag3,tag5,tag6)'];
 
-    def test_split_top_level_hed_string_tags(self):
+    def test_split_hed_string(self):
         hed_string_delimiter = HedStringDelimiter(self.mixed_hed_string);
-        top_level_tag_set = hed_string_delimiter.split_top_level_hed_string_tags();
+        top_level_tag_set = hed_string_delimiter.split_hed_string();
         self.assertTrue(top_level_tag_set);
         self.assertIsInstance(top_level_tag_set, set);
+
+    def test_get_tag_set(self):
+        hed_string_delimiter = HedStringDelimiter(self.mixed_hed_string);
+        tag_set = hed_string_delimiter.get_tag_set();
+        self.assertTrue(tag_set);
+        self.assertIsInstance(tag_set, set);
+
+    # def test_remove_elements_from_set(self):
+    #     hed_string_delimiter = HedStringDelimiter(self.mixed_hed_string);
+    #     top_level_tags = hed_string_delimiter.split_hed_string();
+    #     set_with_removed_elements = hed_string_delimiter.remove_elements_from_set(self.mixed_hed_string,
+    #                                                                               self.removal_elements);
+    #     self.assertTrue(set_with_removed_elements);
+    #     self.assertIsInstance(set_with_removed_elements, set);
 
 if __name__ == '__main__':
     unittest.main();
