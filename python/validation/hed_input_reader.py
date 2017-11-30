2925dc62af0ef91c7095b7e5ec6e571b5f11d6d1
jcockfie <jeremy.cockfield@gmail.com>
2017-11-30 12:11:32 -0600

    fixed formatting in get_hed_string_from_text_file_row function

--- python/validation/hed_input_reader.py
+++ python/validation/hed_input_reader.py
@@ -377,9 +377,10 @@ class HedInputReader:
             row_hed_tags = split_row[hed_tag_column];
             if row_hed_tags:
                 if hed_tag_column in prefixed_needed_tag_columns:
-                    row_hed_tags = HedInputReader.prepend_paths_to_prefixed_needed_tag_columns(row_hed_tags,
-                                                                                               prefixed_needed_tag_columns,
-                                                                                               hed_tag_column);
+                    row_hed_tags = HedInputReader.prepend_paths_to_prefixed_needed_tag_columns(
+                        row_hed_tags,
+                        prefixed_needed_tag_columns,
+                        hed_tag_column);
                 hed_tags.append(row_hed_tags);
         return ','.join(hed_tags);
 
