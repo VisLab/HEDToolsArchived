6b1428c5b4588d283cc609b8d16b2bf912a64871
jcockfie <jeremy.cockfield@gmail.com>
2017-12-01 10:35:37 -0600

    renamed csv_file_examples to hed_file_examples

--- python/examples/csv_file_examples.py
+++
@@ -1,8 +0,0 @@
-from validation.hed_input_reader import HedInputReader
-
-if __name__ == '__main__':
-    spreadsheet_path = '../tests/data/TX14 HED Tags v9.87.csv';
-    prefixed_needed_tag_columns = {2: 'Long', 3: 'Description', 4: 'Label', 5: 'Category', 7: 'Attribute'}
-    hed_input_reader = HedInputReader(spreadsheet_path, tag_columns=[6],
-                                      prefixed_needed_tag_columns=prefixed_needed_tag_columns);
-    print('TX14 HED Tags v9.87.csv validation issues:\n' + hed_input_reader.get_validation_issues());
