2ad60b5f38a750a34677e2481780b2e42649286a
jcockfie <jeremy.cockfield@gmail.com>
2017-12-01 17:42:09 -0600

    added unit test for get_file_extension function

---
+++ python/tests/test_hed_input_reader.py
@@ -0,0 +1,16 @@
+import unittest;
+from validation.hed_input_reader import HedInputReader;
+
+
+class Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.file_with_extension = 'file_with_extension.txt';
+
+    def test_get_file_extension(self):
+        file_extension = HedInputReader.get_file_extension(self.file_with_extension);
+        self.assertIsInstance(file_extension, basestring);
+        self.assertTrue(file_extension);
+
+if __name__ == '__main__':
+    unittest.main();
