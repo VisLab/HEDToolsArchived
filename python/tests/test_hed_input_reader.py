1ed753de2f144ef4a010ddd27ca274575adf8215
jcockfie <jeremy.cockfield@gmail.com>
2017-12-02 12:11:04 -0600

    added unit test for subtract_1_from_list_elements function

--- python/tests/test_hed_input_reader.py
+++ python/tests/test_hed_input_reader.py
@@ -7,6 +7,7 @@ class Test(unittest.TestCase):
     def setUpClass(cls):
         cls.file_with_extension = 'file_with_extension.txt';
         cls.integer_key_dictionary = {1: 'one', 2: 'two', 3: 'three'};
+        cls.integer_list = [1, 2, 3];
 
     def test_get_file_extension(self):
         file_extension = HedInputReader.get_file_extension(self.file_with_extension);
@@ -26,6 +27,15 @@ class Test(unittest.TestCase):
         new_dictionary_key_sum = sum(one_subtracted_key_dictionary.keys());
         original_dictionary_key_length = len(self.integer_key_dictionary.keys());
         self.assertEqual(original_dictionary_key_sum - new_dictionary_key_sum, original_dictionary_key_length);
+
+    def test_subtract_1_from_list_elements(self):
+        one_subtracted_list = HedInputReader.subtract_1_from_list_elements(self.integer_list);
+        self.assertIsInstance(one_subtracted_list, list);
+        self.assertTrue(one_subtracted_list);
+        original_list_sum = sum(self.integer_list);
+        new_list_sum = sum(one_subtracted_list);
+        original_list_length = len(self.integer_list);
+        self.assertEqual(original_list_sum - new_list_sum, original_list_length);
 
 
 
