a1ad235b83d8965ec4c283ef7def55f2170f3061
jcockfie <jeremy.cockfield@gmail.com>
2018-01-04 12:40:22 -0600

    changed test function

--- matlab/tests/test_validatehedstr.m
+++ matlab/tests/test_validatehedstr.m
@@ -23,7 +23,7 @@ values.HEDString3 = ['(Participant ~ ' ...
 function teardown(values) %#ok<INUSD,DEFNU>
 % Function executed after each test
 
-function testCheckGroupBrackets(values)  %#ok<DEFNU>
+function testValidateHedStr(values)  %#ok<DEFNU>
 % Unit test for editmaps
 fprintf('\nUnit tests for checkgroupbrackets\n');
 
