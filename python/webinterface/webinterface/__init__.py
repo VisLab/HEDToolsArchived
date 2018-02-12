210a3f48bc180a85fe7c5b9544e49cc0db8e543f
jcockfie <jeremy.cockfield@gmail.com>
2018-02-12 11:42:29 -0600

    upload directory and logging is setup before app is launched

--- python/webinterface/webinterface/__init__.py
+++ python/webinterface/webinterface/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask;
-
 app = Flask(__name__);
 app.config.from_object('config.Config');
 from webinterface.views import *;
+from webinterface.utils import *;
+setup_logging();
+setup_upload_directory();
 
 if __name__ == '__main__':
     app.run();
