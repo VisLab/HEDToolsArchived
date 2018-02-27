274f7272b4f26c743870fc76fc6aeb505e507138
jcockfie <jeremy.cockfield@gmail.com>
2018-02-27 17:52:48 -0600

    added unit tests for ErrorReporter class

---
+++ matlab/tests/validation/ErrorReporterTest.m
@@ -0,0 +1,34 @@
+classdef ErrorReporterTest < matlab.unittest.TestCase
+    
+    methods (Test)
+        
+        function testGetErrorTypeMap(testCase)
+            % Unit test for getErrorTypeMap function
+            errorReporter = ErrorReporter();
+            errorTypeMap = errorReporter.getErrorTypeMap();
+            testCase.assertInstanceOf(errorTypeMap, 'containers.Map');
+        end % testGetErrorTypeMap
+        
+        function testPopulateErrorTypeMap(testCase)
+            % Unit test for populateErrorTypeMap function
+            errorTypeMap = ErrorReporter.populateErrorTypeMap();
+            testCase.assertInstanceOf(errorTypeMap, 'containers.Map');
+        end % testPopulateErrorTypeMap
+        
+        function testCountPlaceHoldersInError(testCase)
+            % Unit test for countPlaceHoldersInError function
+            errorReporter = ErrorReporter();
+            errors = ErrorConstants.ErrorMessages;
+            numberOfErrors = length(errors);
+            for a = 1:numberOfErrors
+                fprintf(errors{a})
+                placeHolderCount = ...
+                    errorReporter.countPlaceHoldersInError(errors{a});
+                testCase.assertInstanceOf(placeHolderCount, 'double');
+            end
+        end % testCountPlaceHoldersInError
+        
+    end % test functions
+    
+end % ErrorReporterTest
+
