aee23af1785578a2a406f69d510ccae99a2ab485
jcockfie <jeremy.cockfield@gmail.com>
2018-03-05 16:45:41 -0600

    added unit test for delete_file_in_upload_directory

--- python/webinterface/webinterface/tests/test_views.py
+++ python/webinterface/webinterface/tests/test_views.py
@@ -1,6 +1,7 @@
 import unittest;
 from flask import Flask;
 
+
 class Test(unittest.TestCase):
 
     def setUp(self):
@@ -12,7 +13,11 @@ class Test(unittest.TestCase):
 
     def test_render_main_page(self):
         response = self.app.get('/');
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200);
+
+    def test_delete_file_in_upload_directory(self):
+        response = self.app.get('/delete/file_that_does_not_exist');
+        self.assertEqual(response.status_code, 404)
 
 if __name__ == '__main__':
     unittest.main();
