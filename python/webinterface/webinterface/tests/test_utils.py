ae19a2959705acd572fa6e4fa798f413bd58296f
jcockfie <jeremy.cockfield@gmail.com>
2018-03-08 12:25:21 -0600

    added unit test for _file_extension_is_valid

--- python/webinterface/webinterface/tests/test_utils.py
+++ python/webinterface/webinterface/tests/test_utils.py
@@ -1,18 +1,19 @@
 import unittest;
-from webinterface.utils import *;
-from StringIO import StringIO;
-from flask import Request;
+from webinterface import utils;
 
 
 class Test(unittest.TestCase):
-
     def setUp(self):
-        self.error_key = 'error';
         self.major_version_key = 'major_versions';
 
     def test_find_major_hed_versions(self):
-        hed_info = find_major_hed_versions();
+        hed_info = utils.find_major_hed_versions();
         self.assertTrue(self.major_version_key in hed_info);
+
+    def test_file_extension_is_valid(self):
+        file_name = 'abc.' + utils.SPREADSHEET_FILE_EXTENSIONS[0];
+        is_valid = utils._file_extension_is_valid(file_name, utils.SPREADSHEET_FILE_EXTENSIONS);
+        self.assertTrue(is_valid);
 
 
 if __name__ == '__main__':
