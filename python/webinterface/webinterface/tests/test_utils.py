df1690011b3b34c89e847499bec862fe9f8cc9f9
jcockfie <jeremy.cockfield@gmail.com>
2018-03-08 13:37:42 -0600

    added unit test for get_validation_issue_count function

--- python/webinterface/webinterface/tests/test_utils.py
+++ python/webinterface/webinterface/tests/test_utils.py
@@ -1,10 +1,12 @@
 import unittest;
 from webinterface import utils;
+import os;
 
 
 class Test(unittest.TestCase):
     def setUp(self):
         self.major_version_key = 'major_versions';
+        self.hed_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/HED.xml');
 
     def test_find_major_hed_versions(self):
         hed_info = utils.find_major_hed_versions();
@@ -15,6 +17,9 @@ class Test(unittest.TestCase):
         is_valid = utils._file_extension_is_valid(file_name, utils.SPREADSHEET_FILE_EXTENSIONS);
         self.assertTrue(is_valid);
 
+    def test_get_validation_issue_count(self):
+        issue_count = utils._get_validation_issue_count('');
+        self.assertEqual(issue_count, 0);
 
 if __name__ == '__main__':
     unittest.main();
