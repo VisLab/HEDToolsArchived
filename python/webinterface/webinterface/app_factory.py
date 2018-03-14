013f136005822b39c8aa9e7ca78ba9cee50da415
jcockfie <jeremy.cockfield@gmail.com>
2018-03-14 17:02:47 -0500

    removed default config file

--- python/webinterface/webinterface/app_factory.py
+++ python/webinterface/webinterface/app_factory.py
@@ -3,7 +3,8 @@ from flask import Flask;
 
 class AppFactory:
     @staticmethod
-    def create_app(config_file='config.ProductionConfig'):
+    def create_app(config_file):
         app = Flask(__name__);
         app.config.from_object(config_file);
         return app;
+
