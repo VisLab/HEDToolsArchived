4cc1743105e21d4f60abeb6a59e2e018a5639d12
jcockfie <jeremy.cockfield@gmail.com>
2018-03-14 17:03:55 -0500

    specified test config file

--- python/webinterface/tests/test_views.py
+++ python/webinterface/tests/test_views.py
@@ -1,17 +1,15 @@
 import unittest;
-from flask import Flask;
+from webinterface.app_factory import AppFactory;
 
 
 class Test(unittest.TestCase):
 
     def setUp(self):
-        app = Flask(__name__);
-        app.config.from_object('config.TestConfig');
+        app = AppFactory.create_app('config.TestConfig');
         from webinterface.views import *;
-        from webinterface.utils import *;
         self.app = app.test_client();
 
-    def test_render_main_page(self):
+    def test_render_home_page(self):
         response = self.app.get('/');
         self.assertEqual(response.status_code, 200);
 
