a88531ff304c42ac4be7356e47657519a74758b2
jcockfie <jeremy.cockfield@gmail.com>
2018-03-22 17:30:12 -0500

    upload dir setup and logging setup are moved outsided of main

--- python/webinterface/webinterface/runserver.py
+++ python/webinterface/webinterface/runserver.py
@@ -4,8 +4,8 @@ from flask_wtf.csrf import CSRFProtect;
 app = AppFactory.create_app('config.ProductionConfig');
 CSRFProtect(app);
 from webinterface.views import *;
+utils.setup_logging();
+utils.setup_upload_directory();
 
 if __name__ == '__main__':
-    utils.setup_logging();
-    utils.setup_upload_directory();
     app.run();
