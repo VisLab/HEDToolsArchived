69416ec4f583fdcb042b89eb5744e864fbc5d769
jcockfie <jeremy.cockfield@gmail.com>
2018-03-23 17:09:01 -0500

    moved setup_logging function to here and added app blueprint for views

--- python/webinterface/webinterface/runserver.py
+++ python/webinterface/webinterface/runserver.py
@@ -1,11 +1,27 @@
+import os;
 from webinterface.app_factory import AppFactory;
+from webinterface import utils;
 from flask_wtf.csrf import CSRFProtect;
+from webinterface.views import view_routes;
+from logging.handlers import RotatingFileHandler;
+from logging import ERROR;
+
+
+def setup_logging():
+    """Sets up the current_application logging. If the log directory does not exist then there will be no logging.
+
+    """
+    if not app.debug and os.path.exists(app.config['LOG_DIRECTORY']):
+        file_handler = RotatingFileHandler(app.config['LOG_FILE'], maxBytes=10 * 1024 * 1024, backupCount=5);
+        file_handler.setLevel(ERROR);
+        app.logger.addHandler(file_handler);
+
 
 app = AppFactory.create_app('config.ProductionConfig');
 CSRFProtect(app);
-from webinterface.views import *;
-utils.setup_logging();
-utils.setup_upload_directory();
+app.register_blueprint(view_routes);
+utils.create_upload_directory(app.config['UPLOAD_FOLDER']);
+setup_logging();
 
 if __name__ == '__main__':
     app.run();
