428411ac67a8fea6a008b9db7ed7dfbd9af16990
jcockfie <jeremy.cockfield@gmail.com>
2018-04-02 14:38:09 -0500

    removed static_url_path

--- python/webinterface/webinterface/app_factory.py
+++ python/webinterface/webinterface/app_factory.py
@@ -4,8 +4,8 @@ from flask_wtf.csrf import CSRFProtect;
 
 class AppFactory:
     @staticmethod
-    def create_app(config_file, static_url_path='/static'):
-        app = Flask(__name__, static_url_path=static_url_path);
+    def create_app(config_file):
+        app = Flask(__name__);
         app.config.from_object(config_file);
         CSRFProtect(app);
         return app;
