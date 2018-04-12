a2dc7efd0dbb0aa108208812494ab05a32ac5dd3
jcockfie <jeremy.cockfield@gmail.com>
2018-04-12 16:30:22 -0500

    updated deploy script

--- python/webinterface/deploy/deploy.sh
+++ python/webinterface/deploy/deploy.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-BUILD_TAG_NAME = "hedtools-validation:latest"
-GIT_REPO_URL = "https://github.com/VisLab/HEDTools"
-GIT_REPO_BRANCH = "develop"
-HOST_PORT = 5000;
-CONTAINER_PORT = 80;
-DEPLOY_DIR = "HEDTools/python/webinterface/deploy/"
-CODE_DEPLOY_DIR = $DEPLOY_DIR + "hedtools"
-CONFIG_FILE = $DEPLOYDIR + "config.py"
-WEBINTERFACE_CODE_DIR = "HEDTools/python/webinterface/webinterface/"
-VALIDATOR_CODE_DIR = "HEDTools/python/hedvalidation/hedvalidation/"
+GIT_DIR="${PWD}/HEDTools"
+IMAGE_NAME="hedtools-validation:latest"
+CONTAINER_NAME="hedtools-validation"
+GIT_REPO_URL="https://github.com/VisLab/HEDTools"
+GIT_REPO_BRANCH="develop"
+HOST_PORT=5000;
+CONTAINER_PORT=80;
+DEPLOY_DIR="HEDTools/python/webinterface/deploy"
+CODE_DEPLOY_DIR="${DEPLOY_DIR}/hedtools"
+CONFIG_FILE="${DEPLOY_DIR}/config.py"
+WSGI_FILE="${DEPLOY_DIR}/webinterface.wsgi"
+WEBINTERFACE_CODE_DIR="HEDTools/python/webinterface/webinterface/"
+VALIDATOR_CODE_DIR="HEDTools/python/hedvalidation/hedvalidation/"
 git clone $GIT_REPO_URL -b $GIT_REPO_BRANCH
-cd $DEPLOY_DIR
 mkdir $CODE_DEPLOY_DIR
 cp $CONFIG_FILE $CODE_DEPLOY_DIR
-cp $WEBINTERFACE_CODE_DIR $CODE_DEPLOY_DIR
-cp $VALIDATOR_CODE_DIR $CODE_DEPLOY_DIR
-docker build -t $BUILD_TAG_NAME
-docker run --restart=always -d -p $HOST_PORT:$CONTAINER_PORT $BUILD_TAG_NAME
-rm -r HEDTools
+cp $WSGI_FILE $CODE_DEPLOY_DIR
+cp -r $WEBINTERFACE_CODE_DIR $CODE_DEPLOY_DIR
+cp -r $VALIDATOR_CODE_DIR $CODE_DEPLOY_DIR
+cd $DEPLOY_DIR
+echo Building new containing...
+docker build -t $IMAGE_NAME .
+echo Deleting old container...
+docker rm -f $CONTAINER_NAME
+docker run --restart=always --name $CONTAINER_NAME -d -p $HOST_PORT:$CONTAINER_PORT $IMAGE_NAME
+rm -rf $GIT_DIR
