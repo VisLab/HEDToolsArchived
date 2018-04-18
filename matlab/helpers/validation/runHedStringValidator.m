b59571f8ddd1bbd16399093d8dd8b4a5403602a5
jcockfie <jeremy.cockfield@gmail.com>
2018-04-18 17:05:13 -0500

    implemented runHedStringValidator function

---
+++ matlab/helpers/validation/runHedStringValidator.m
@@ -0,0 +1,39 @@
+% Runs the validators on the HED string. If this is passed then all the
+% other validators are run on the tags and groups in the HED string.
+% Usage:
+%
+%   >>  errors = runHedStringValidator(hedString)
+%
+% Input:
+%
+%   hedString
+%                   A HED string. 
+%
+% Output:
+%
+%   errors
+%                   A string containing the validation errors.
+%
+% Copyright (C) 2012-2016 Thomas Rognon tcrognon@gmail.com,
+% Jeremy Cockfield jeremy.cockfield@gmail.com, and
+% Kay Robbins kay.robbins@utsa.edu
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+function errors = runHedStringValidator(hedString)
+errors = '';
+errors = [errors checkgroupbrackets(hedString)];
+errors = [errors checkcommas(hedString)];
+end % runHedStringValidator
