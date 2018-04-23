0b925c1753bf6616b4ff0578d48b35da1b088d57
jcockfie <jeremy.cockfield@gmail.com>
2018-04-23 16:42:22 -0500

    added unit tests for appendHedTagPrefixes

---
+++ matlab/tests/validation/appendHedTagPrefixesTest.m
@@ -0,0 +1,19 @@
+function tests = appendHedTagPrefixesTest
+tests = functiontests(localfunctions);
+end % errorReporterTest
+
+function setupOnce(testCase)
+testCase.TestData.hedTagArray = {'a','b','c'};
+testCase.TestData.specificColumns.label = 1;
+testCase.TestData.labelPrefix = 'Event/Label/';
+end % setupOnce
+
+
+%% Test Functions
+function basicInputTest(testCase)
+hedTagArray = appendHedTagPrefixes(testCase.TestData.hedTagArray, ...
+    testCase.TestData.specificColumns);
+testCase.verifyClass(hedTagArray, 'cell');
+expected = {[testCase.TestData.labelPrefix 'a'],'b','c'};
+testCase.verifyEqual(hedTagArray,expected)
+end % basicInputTest
