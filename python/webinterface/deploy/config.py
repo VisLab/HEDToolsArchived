b530aad82b60698835f6dacc1483961400d4c755
Jeremy Cockfield <jeremy.cockfield@gmail.com>
2018-04-24 11:27:53 -0500

    Update config.py

--- python/webinterface/deploy/config.py
+++ python/webinterface/deploy/config.py
@@ -12,7 +12,7 @@ import tempfile;
 class ProductionConfig(object):
     UPLOAD_FOLDER = os.path.join(tempfile.gettempdir(), 'hedtools_uploads');
     SECRET_KEY = os.urandom(24);
-    LOG_DIRECTORY = '/var/log/hedtools';
+    LOG_DIRECTORY = '/dev/stdout';
     LOG_FILE = os.path.join(LOG_DIRECTORY, 'error.log');
     TESTING = False;
     DEBUG = False;
