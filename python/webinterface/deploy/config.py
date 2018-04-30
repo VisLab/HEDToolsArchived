a531c25dad721d2c3ca16c37f1404b95f86a9c94
Jeremy Cockfield <jeremy.cockfield@gmail.com>
2018-04-30 12:44:21 -0500

    Removed ProductionConfig class

--- python/webinterface/deploy/config.py
+++ python/webinterface/deploy/config.py
@@ -8,18 +8,7 @@ Created on Dec 21, 2017
 import os;
 import tempfile;
 
-
-class ProductionConfig(object):
-    UPLOAD_FOLDER = os.path.join(tempfile.gettempdir(), 'hedtools_uploads');
-    SECRET_KEY = '2k3jsd90sf24*(#&#J@#KJ@#(U@#($(@*)';
-    LOG_DIRECTORY = '/var/log/hedtools';
-    LOG_FILE = os.path.join(LOG_DIRECTORY, 'error.log');
-    TESTING = False;
-    DEBUG = False;
-    URL_PREFIX = '/hed';
-    STATIC_URL_PATH = '/hed/static';
-
-class LocalConfig(object):
+class DevelopmentConfig(object):
     UPLOAD_FOLDER = os.path.join(tempfile.gettempdir(), 'hedtools_uploads');
     SECRET_KEY = os.urandom(24);
     LOG_DIRECTORY = '/var/log/hedtools';
