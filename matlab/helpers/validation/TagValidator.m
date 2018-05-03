ae7691a81d976eff94182ef19b6d0d1e5b714dc0
jcockfie <jeremy.cockfield@gmail.com>
2018-05-03 12:59:46 -0500

    fixed checkCommas to work with unit tests

--- matlab/helpers/validation/TagValidator.m
+++ matlab/helpers/validation/TagValidator.m
@@ -1,5 +1,5 @@
 % This class contains the interface that does the actual validation and
-% return any issues found. 
+% return any issues found.
 %
 % Copyright (C) 2015 Jeremy Cockfield jeremy.cockfield@gmail.com and
 % Kay Robbins, UTSA, kay.robbins@utsa.edu
@@ -27,6 +27,7 @@ classdef TagValidator
     properties(Constant, Access=private)
         capWarnings = 'cap';
         capExpression = '^[a-z]|/[a-z]|[^|]\s+[A-Z]';
+        commaError = 'comma';
     end % Instance properties
     
     methods
@@ -46,6 +47,45 @@ classdef TagValidator
                     originalTag);
             end
         end % runHedStringValidator
+        
+        function errors = checkCommas(obj, hedString)
+            errors = '';
+            currentTag = '';
+            lastNonEmptyCharacter = '';
+            hedStringLength = length(hedString);
+            characterIndex = 1;
+            while characterIndex < hedStringLength
+                character = hedString(characterIndex);
+                currentTag = [currentTag character]; %#ok<AGROW>
+                if ~TagValidator.characterIsWhitespace(character)
+                    if TagValidator.characterIsDelimiter(character)
+                        currentTag = '';
+                    end
+                    if  character == '(' && ...
+                            obj.isValidTagWithParentheses(...
+                            hedString, currentTag, characterIndex)
+                        characterIndex = ...
+                            obj.getIndexAtEndOfParentheses(...
+                            hedString, currentTag, characterIndex);
+                        currentTag = '';
+                    elseif obj.commaMissingBeforeOpeningBracket(...
+                            lastNonEmptyCharacter, character)
+                        currentTag = strtrim(currentTag(1:end-1));
+                        errors = generateerror(obj.commaError, [], ...
+                            currentTag);
+                        break;
+                    elseif obj.commaIsMissingAfterClosingBracket(...
+                            lastNonEmptyCharacter, character)
+                        currentTag = strtrim(currentTag(1:end-1));
+                        errors = generateerror(obj.commaError, [], ...
+                            currentTag);
+                        break;
+                    end
+                    lastNonEmptyCharacter = character;
+                end
+                characterIndex = characterIndex + 1;
+            end
+        end % checkcommas
         
     end % Public methods
     
@@ -71,7 +111,93 @@ classdef TagValidator
                 end
             end
         end % checkIfParentTagTakesValue
-           
+        
+        
+        
+        
+        
+        
+        function tagIsValid = isValidTagWithParentheses(obj, hedString, ...
+                currentTag, characterIndex)
+            % Checks to see if the current tag with the next set of
+            % parentheses in the HED string is valid.
+            currentTag = currentTag(1:end-1);
+            restOfHedString = hedString(characterIndex:end);
+            currentTagWithParentheses = ...
+                obj.getNextSetOfParenthesesInHedString([currentTag ...
+                restOfHedString]);
+            currentTagWithParentheses = lower(currentTagWithParentheses);
+            tagIsValid = obj.hedMaps.tags.isKey(currentTagWithParentheses);
+        end % isValidTagWithParentheses
+        
+        
+        function parenthesesLength = getIndexAtEndOfParentheses(...
+                obj, hedString, currentTag, characterIndex)
+            % Checks to see if the current tag with the next set of
+            % parentheses in the HED string is valid. Some tags have
+            % parentheses and this function is implemented to avoid
+            % reporting a missing comma error.
+            currentTag = currentTag(1:end-1);
+            restOfHedString = hedString(characterIndex:end);
+            [~, parenthesesLength] = ...
+                obj.getNextSetOfParenthesesInHedString(...
+                [currentTag restOfHedString]);
+        end % getIndexAtEndOfParentheses
+        
+        function [setOfParentheses, parenthesesLength] = ...
+                getNextSetOfParenthesesInHedString(obj, hedString) %#ok<INUSL>
+            % Gets the next set of parentheses in the provided HED string.
+            setOfParentheses = '';
+            openingParenthesisFound = false;
+            numberOfCharacters = length(hedString);
+            parenthesesLength = 0;
+            for parenthesesLength = 1:numberOfCharacters
+                character = hedString(parenthesesLength);
+                setOfParentheses = [setOfParentheses ...
+                    hedString(parenthesesLength)]; %#ok<AGROW>
+                if character == '('
+                    openingParenthesisFound = true;
+                elseif character == ')' && openingParenthesisFound
+                    return;
+                end
+            end
+        end % getNextSetOfParenthesesInHedString
+        
+        function commaMissing = commaIsMissingAfterClosingBracket(...
+                obj, lastNonEmptyCharacter, currentCharacter)
+            % Returns true if a comma is missing after a closing bracket.
+            commaMissing = ~isempty(lastNonEmptyCharacter) && ...
+                lastNonEmptyCharacter == ')' && ...
+                ~obj.characterIsDelimiter(currentCharacter);
+        end % commaIsMissingAfterClosingBracket
+        
+        function commaMissing = commaMissingBeforeOpeningBracket(...
+                obj, lastNonEmptyCharacter, currentCharacter)
+            % Returns true if a comma is missing before an opening bracket.
+            commaMissing = ~isempty(lastNonEmptyCharacter) && ...
+                ~obj.characterIsDelimiter(lastNonEmptyCharacter) && ...
+                currentCharacter == '(';
+        end % commaMissingBeforeOpeningBracket
+        
     end % Private methods
+    
+    methods(Static)
+        function isWhitespace = characterIsWhitespace(character)
+            % Checks to see if the specified character is whitespace. Tab
+            % and newline characters are considered whitespace.
+            isWhitespace = ~isempty(regexp(character, '[\\n\\t ]', ...
+                'once'));
+        end % characterIsWhitespace
+        
+        function isDelimiter = characterIsDelimiter(character)
+            % Checks to see if the specified character is a delimiter.
+            % Comma and tildes are considered delimiters.
+            isDelimiter = ~isempty(regexp(character, '[,~]', 'once'));
+        end % characterIsDelimiter
+        
+    end % Static methods
+    
+    
+    
     
 end % TagValidator
