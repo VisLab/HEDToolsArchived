3c26744d407a3100e966e849c6e464a30a53b696
jcockfie <jeremy.cockfield@gmail.com>
2018-05-03 13:17:29 -0500

    added unit tests for checkGroupBrackets

--- matlab/tests/validation/TagValidatorTest.m
+++ matlab/tests/validation/TagValidatorTest.m
@@ -7,11 +7,16 @@ load('hedMaps.mat');
 testCase.TestData.tagValidator = TagValidator(hedMaps);
 testCase.TestData.validCapsTag = 'This is/A/Valid tag';
 testCase.TestData.invalidCapsTag = 'This Is/A/invalid tag';
-testCase.TestData.validHedStringWithCommas = '/This/is/a/tag, /This/is/another/tag';
+testCase.TestData.validHedStringWithCommas = ...
+    '/This/is/a/tag, /This/is/another/tag';
 testCase.TestData.commaMissingBeforeOpeningParenthesis = ...
     '/This/is/a/tag (/This/is/another/tag';
 testCase.TestData.commaMissingAfterClosingParenthesis = ...
     '(/This/is/a/tag) /This/is/another/tag';
+testCase.TestData.validGroupWithEqualBrackets = ...
+    '(/This/is/a/tag, /This/is/another/tag)';
+testCase.TestData.validGroupWithUnequalBrackets = ...
+    '(/This/is/a/tag, /This/is/another/tag';
 end % setupOnce
 
 
@@ -34,6 +39,15 @@ errors = testCase.TestData.tagValidator.checkCommas(...
 testCase.verifyNotEmpty(errors);
 errors = testCase.TestData.tagValidator.checkCommas(...
     testCase.TestData.commaMissingAfterClosingParenthesis);
+testCase.verifyNotEmpty(errors);
+end
+
+function checkGroupBracketsTest(testCase)
+errors = testCase.TestData.tagValidator.checkGroupBrackets(...
+    testCase.TestData.validGroupWithEqualBrackets);
+testCase.verifyEmpty(errors);
+errors = testCase.TestData.tagValidator.checkGroupBrackets(...
+    testCase.TestData.validGroupWithUnequalBrackets);
 testCase.verifyNotEmpty(errors);
 end
 
