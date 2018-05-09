d931deb04451388d6ce1b9932d697dd98e99b2b7
jcockfie <jeremy.cockfield@gmail.com>
2018-05-09 10:26:10 -0500

    added unit tests for getHedMapsTest

---
+++ matlab/tests/validation/getHedMapsTest.m
@@ -0,0 +1,14 @@
+function tests = getHedMapsTest
+tests = functiontests(localfunctions);
+end % errorReporterTest
+
+function setupOnce(testCase)
+
+end % setupOnce
+
+
+%% Test Functions
+function testBasicInput(testCase)
+hedMaps = getHedMaps();
+testCase.assertInstanceOf(hedMaps, 'struct');
+end % testErrorReporter
