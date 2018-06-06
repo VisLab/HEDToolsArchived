aabbbe97c6e96f5dfe17ddce16ad754d044c58a4
jcockfie <jeremy.cockfield@gmail.com>
2018-06-06 12:38:36 -0500

    fixed spacing

--- python/hedemailer/hedemailer/routes.py
+++ python/hedemailer/hedemailer/routes.py
@@ -5,6 +5,7 @@ import json;
 app_config = current_app.config;
 route_blueprint = Blueprint('route_blueprint', __name__);
 
+
 @route_blueprint.route('/', methods=['POST'])
 def process_gollum_event():
     try:
