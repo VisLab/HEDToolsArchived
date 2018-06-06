284d373e353b71399fe8147a5533aead5cd8911b
jcockfie <jeremy.cockfield@gmail.com>
2018-06-06 15:50:35 -0500

    added test cases for routes

---
+++ python/hedemailer/tests/test_routes.py
@@ -0,0 +1,19 @@
+import unittest;
+from hedemailer.app_factory import AppFactory;
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        app = AppFactory.create_app('config.TestConfig');
+        with app.app_context():
+            from hedemailer.routes import route_blueprint;
+            app.register_blueprint(route_blueprint);
+            self.app = app.test_client();
+
+    def test_render_home_page(self):
+        response = self.app.post('/');
+        self.assertEqual(response.status_code, 200);
+
+
+if __name__ == '__main__':
+    unittest.main();
