ecaf74db9d0ebc1858f62835d3427e8035dc7622
jcockfie <jeremy.cockfield@gmail.com>
2018-06-06 15:49:31 -0500

    added more descriptive messages

--- python/hedemailer/hedemailer/routes.py
+++ python/hedemailer/hedemailer/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, current_app, request;
-from hedemailer import hed_emailer;
+from hedemailer import hed_emailer, utils;
 import json;
 
 app_config = current_app.config;
@@ -9,8 +9,13 @@ route_blueprint = Blueprint('route_blueprint', __name__);
 @route_blueprint.route('/', methods=['POST'])
 def process_hed_payload():
     try:
-        if hed_emailer.request_is_github_gollum_event(request):
+        if utils.request_is_github_gollum_event(request):
             hed_emailer.send_email(request);
+            return json.dumps({'success': True, 'message': 'Email(s) correctly sent'}), 200, {
+                'ContentType': 'application/json'};
     except Exception as ex:
-        return json.dumps({'success': False, 'message': ex.message}), 500, {'ContentType': 'application/json'};
-    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'};
+        return json.dumps({'success': False, 'message': ex}), 500, {'ContentType': 'application/json'};
+    return json.dumps({'success': True,
+                       'message': 'No email(s) sent. Not in the correct format. Content type needs to'
+                                  ' be in JSON and X-GitHub-Event must be gollum'}), 200, {
+               'ContentType': 'application/json'};
