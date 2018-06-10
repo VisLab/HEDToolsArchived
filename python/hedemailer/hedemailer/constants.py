1b825a031955d829f51783ecc97c7f5e0515dca1
jcockfie <jeremy.cockfield@gmail.com>
2018-06-10 00:18:44 -0500

    changed constants.NO_EMAILS_SENT to NO_EMAILS_SENT

--- python/hedemailer/hedemailer/constants.py
+++ python/hedemailer/hedemailer/constants.py
@@ -4,7 +4,7 @@ EMAILS_SENT = 'Email(s) correctly sent';
 EMAIL_SENT_RESPONSE = json.dumps({'success': True, 'message': EMAILS_SENT}), 200, {'ContentType': 'application/json'};
 NO_EMAILS_SENT = 'No email(s) sent. Not in the correct format. Content type needs to be in JSON and X-GitHub-Event'
 ' must be gollum';
-NO_EMAILS_SENT_RESPONSE = json.dumps({'success': True, 'message': constants.NO_EMAILS_SENT}), 200, {
+NO_EMAILS_SENT_RESPONSE = json.dumps({'success': True, 'message': NO_EMAILS_SENT}), 200, {
     'ContentType': 'application/json'};
 
 
