d4623d0650d9d3c0213ae5eeac747a5a7e4d922c
jcockfie <jeremy.cockfield@gmail.com>
2018-06-10 15:55:58 -0500

    unit test file is running

--- python/hedemailer/tests/test_hed_emailer.py
+++ python/hedemailer/tests/test_hed_emailer.py
@@ -1,23 +1,28 @@
 import unittest;
 import json;
+from hedemailer.app_factory import AppFactory;
 
 
 class Test(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.hed_payload_file = 'data/hed_payload.json';
-        with open(cls.hed_payload_file) as opened_hed_payload_file:
-            cls.hed_payload_string = json.load(opened_hed_payload_file);
-        cls.hed_emailer =
+        hed_payload_file = 'data/hed_payload.json';
+        Test.create_test_app();
+        Test.get_payload_from_file(hed_payload_file);
 
     @classmethod
-    def tearDownClass(self):
-        self.github_payload_file.close();
+    def get_payload_from_file(cls, hed_payload_file):
+        with open(hed_payload_file) as opened_hed_payload_file:
+            cls.hed_payload_string = json.dumps(json.load(opened_hed_payload_file));
 
-    def test_send_gollum_email(self):
-
-    pass
+    @classmethod
+    def create_test_app(cls):
+        app = AppFactory.create_app('config.TestConfig');
+        with app.app_context():
+            from hedemailer.routes import route_blueprint;
+            app.register_blueprint(route_blueprint);
+            cls.app = app.test_client();
 
 
 if __name__ == "__main__":
