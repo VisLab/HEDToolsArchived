6aed2538200c53448b46f44756b69ad228272307
jcockfie <jeremy.cockfield@gmail.com>
2018-06-13 17:46:04 -0500

    added unit tests for test_delete_file_if_exist

--- python/hedemailer/tests/test_utils.py
+++ python/hedemailer/tests/test_utils.py
@@ -10,6 +10,8 @@ class Test(unittest.TestCase):
     def setUpClass(cls):
         Test.create_test_app();
         hed_payload_file = 'data/hed_payload.json';
+        cls.file_that_exist = 'data/exist.txt';
+        cls.file_that_does_not_exit = 'data/does_not_exist.txt';
         Test.get_payload_from_file(hed_payload_file);
 
     @classmethod
@@ -26,8 +28,8 @@ class Test(unittest.TestCase):
             cls.app = app.test_client();
 
     def create_file_in_data_dir(self, file_path):
-        with open(file_name, 'w') as file_name)
-
+        with open(file_path, 'w') as file:
+            print(file_path + ' created');
 
     def test_wiki_page_is_hed_schema_no_hed_payload(self):
         github_payload_dictionary = {};
@@ -39,8 +41,12 @@ class Test(unittest.TestCase):
         is_hed_schema = hedemailer.utils.wiki_page_is_hed_schema(github_payload_dictionary)
         self.assertFalse(is_hed_schema, 'Wiki page should be HED schema');
 
-    def test_delete_file_if_exist(file_path)
-        delete_file_if_exist(file_path)
+    def test_delete_file_if_exist(self):
+        self.create_file_in_data_dir(self.file_that_exist);
+        file_deleted = hedemailer.utils.delete_file_if_exist(self.file_that_exist);
+        self.assertTrue(file_deleted, 'File should have been deleted');
+        file_deleted = hedemailer.utils.delete_file_if_exist(self.file_that_does_not_exit);
+        self.assertFalse(file_deleted, 'File should not have been deleted. Does not exist');
 
 
 if __name__ == "__main__":
