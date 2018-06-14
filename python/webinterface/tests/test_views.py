546c9d6d9bb294fe5b58af4ed4f8db99bbdcca5f
jcockfie <jeremy.cockfield@gmail.com>
2018-06-14 12:31:54 -0500

    fixed unit tests

--- python/webinterface/tests/test_views.py
+++ python/webinterface/tests/test_views.py
@@ -1,19 +1,25 @@
 import unittest;
 import os;
 from webinterface.app_factory import AppFactory;
+import shutil;
 
 
 class Test(unittest.TestCase):
-    def setUp(self):
-        upload_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/HED.xml');
+    @classmethod
+    def setUpClass(cls):
+        cls.upload_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/upload');
         app = AppFactory.create_app('config.TestConfig');
         with app.app_context():
             from webinterface import utils;
             from webinterface.routes import route_blueprint;
             app.register_blueprint(route_blueprint);
-            utils.create_upload_directory(upload_directory);
-            app.config['UPLOAD_FOLDER'] = upload_directory;
-            self.app = app.test_client();
+            utils.create_upload_directory(cls.upload_directory);
+            app.config['UPLOAD_FOLDER'] = cls.upload_directory;
+            cls.app = app.test_client();
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.upload_directory);
 
     def test_render_home_page(self):
         response = self.app.get('/');
@@ -24,15 +30,15 @@ class Test(unittest.TestCase):
         self.assertEqual(response.status_code, 404);
 
     def test_get_hed_version_in_file(self):
-        response = self.app.post('/gethedversion');
+        response = self.app.post('/get-hed-version');
         self.assertEqual(response.status_code, 400);
 
     def test_get_major_hed_versions(self):
-        response = self.app.post('/getmajorhedversions');
+        response = self.app.post('/get-major-hed-versions');
         self.assertEqual(response.status_code, 405);
 
     def test_get_spreadsheet_columns_info(self):
-        response = self.app.post('/getspreadsheetcolumnsinfo');
+        response = self.app.post('/get-spreadsheet-columns-info');
         self.assertEqual(response.status_code, 400);
 
 
