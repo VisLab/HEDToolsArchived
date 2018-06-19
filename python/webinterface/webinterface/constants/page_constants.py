33ef317c386e272b3b235e730d7675d0d1890129
jcockfie <jeremy.cockfield@gmail.com>
2018-06-19 11:50:29 -0500

    added constant files

---
+++ python/webinterface/webinterface/constants/page_constants.py
@@ -0,0 +1,4 @@
+ADDITIONAL_EXAMPLES_PAGE = 'additional-examples.html';
+HELP_PAGE = 'help.html';
+HOME_PAGE = 'home.html';
+VALIDATION_PAGE = 'validation.html';
---
+++ python/webinterface/webinterface/constants/route_constants.py
@@ -0,0 +1,13 @@
+ADDITIONAL_EXAMPLES_ROUTE = '/additional-examples';
+COMMON_ERRORS = 'common-errors.html';
+COMMON_ERRORS_ROUTE = '/common-errors';
+DELETE_FILE_ROUTE = '/delete/<filename>';
+DOWNLOAD_FILE_ROUTE = '/download/<filename>';
+HED_VERSION_ROUTE = '/get-hed-version';
+HELP_ROUTE ='/help';
+HOME_ROUTE = '/';
+MAJOR_HED_VERSION_ROUTE = '/get-major-hed-versions';
+SPREADSHEET_COLUMN_INFO_ROUTE = '/get-spreadsheet-columns-info';
+SUBMIT_ROUTE = '/submit';
+VALIDATION_ROUTE = '/validation';
+WORKSHEET_COLUMN_INFO = '/get-worksheets-info';
