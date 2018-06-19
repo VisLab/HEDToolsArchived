56de658b927d18ad787d87ae13a0f96a69f9edf7
jcockfie <jeremy.cockfield@gmail.com>
2018-06-19 17:33:42 -0500

    restuctured constants

---
+++ python/webinterface/webinterface/constants/form/html_form_constants.py
@@ -0,0 +1,7 @@
+CHECK_FOR_WARNINGS = 'generate-warnings';
+COLUMN_POSTFIX = '-column';
+HAS_COLUMN_NAMES = 'has-column-names';
+HED_VERSION = 'hed-version';
+LEAF_EXTENSIONS = 'leaf-extensions';
+TAG_COLUMNS = 'tag-columns';
+WORKSHEET_NAME = 'worksheet';
