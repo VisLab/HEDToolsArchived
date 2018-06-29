c00d46cdd69b1626cd668fc53ea74171a4e7465c
jcockfie <jeremy.cockfield@gmail.com>
2018-06-29 11:23:43 -0500

    added usertagsTest test cases

--- matlab/tests/findEEGHedEventsTest.m
+++ matlab/tests/findEEGHedEventsTest.m
@@ -3,14 +3,26 @@ tests = functiontests(localfunctions);
 end % findEEGHedEventsTest
 
 function setupOnce(testCase)
-emptyEventsEEG = [];
-emptyEventsEEG.event = [];
-testCase.TestData.emptyEventsEEG = emptyEventsEEG;
+testCase.TestData.emptyQueryString = '';
+eegWithNoTags.event = [];
+testCase.TestData.eegWithNoTags = eegWithNoTags;
+eegWithUserTags.event(1).usertags = '/a/b/c, e/f/g';
+eegWithUserTags.event(2).usertags = 'a/b';
+testCase.TestData.eegWithUserTags = eegWithUserTags;
 end
 
 %% Test Functions
 function emptyEEGTest(testCase)
-[matchMask, tags] = findEEGHedEvents(testCase.TestData.emptyEventsEEG, '');
+[matchMask, tags] = findEEGHedEvents(testCase.TestData.eegWithNoTags, ...
+    testCase.TestData.emptyQueryString);
 testCase.verifyEmpty(matchMask);
 testCase.verifyEmpty(tags);
-end % basicConstructorTest
+end % emptyEEGTest
+
+function usertagsTest(testCase)
+[matchMask, tags] = findEEGHedEvents(testCase.TestData.eegWithUserTags, ...
+    testCase.TestData.emptyQueryString);
+testCase.verifyEqual(matchMask, false(1, ...
+    length(testCase.TestData.eegWithUserTags.event)));
+testCase.verifyEmpty(tags);
+end % emptyEEGTest
