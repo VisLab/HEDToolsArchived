39cdcca0a595e2f092100e94a7bba6f98d518349
jcockfie <jeremy.cockfield@gmail.com>
2018-06-29 11:35:13 -0500

    added hedtagsTest test cases

--- matlab/tests/findEEGHedEventsTest.m
+++ matlab/tests/findEEGHedEventsTest.m
@@ -9,6 +9,12 @@ testCase.TestData.eegWithNoTags = eegWithNoTags;
 eegWithUserTags.event(1).usertags = '/a/b/c, e/f/g';
 eegWithUserTags.event(2).usertags = 'a/b';
 testCase.TestData.eegWithUserTags = eegWithUserTags;
+eegWithHedTags.event(1).hedtags = '/a/b/c, e/f/g';
+eegWithHedTags.event(2).hedtags = 'a/b';
+testCase.TestData.eegWithHedTags = eegWithHedTags;
+testCase.TestData.queryString1 = 'a';
+testCase.TestData.queryString2 = 'e/f/g';
+
 end
 
 %% Test Functions
@@ -25,4 +31,30 @@ function usertagsTest(testCase)
 testCase.verifyEqual(matchMask, false(1, ...
     length(testCase.TestData.eegWithUserTags.event)));
 testCase.verifyEmpty(tags);
-end % emptyEEGTest
+[matchMask, tags] = findEEGHedEvents(testCase.TestData.eegWithUserTags, ...
+    testCase.TestData.queryString1);
+testCase.verifyEqual(matchMask, true(1, ...
+    length(testCase.TestData.eegWithUserTags.event)));
+testCase.verifyNotEmpty(tags);
+[matchMask, tags] = findEEGHedEvents(testCase.TestData.eegWithUserTags, ...
+    testCase.TestData.queryString2);
+testCase.verifyEqual(matchMask, [true false]);
+testCase.verifyNotEmpty(tags);
+end % usertagsTest
+
+function hedtagsTest(testCase)
+[matchMask, tags] = findEEGHedEvents(testCase.TestData.eegWithHedTags, ...
+    testCase.TestData.emptyQueryString);
+testCase.verifyEqual(matchMask, false(1, ...
+    length(testCase.TestData.eegWithUserTags.event)));
+testCase.verifyEmpty(tags);
+[matchMask, tags] = findEEGHedEvents(testCase.TestData.eegWithHedTags, ...
+    testCase.TestData.queryString1);
+testCase.verifyEqual(matchMask, true(1, ...
+    length(testCase.TestData.eegWithHedTags.event)));
+testCase.verifyNotEmpty(tags);
+[matchMask, tags] = findEEGHedEvents(testCase.TestData.eegWithHedTags, ...
+    testCase.TestData.queryString2);
+testCase.verifyEqual(matchMask, [true false]);
+testCase.verifyNotEmpty(tags);
+end % usertagsTest
