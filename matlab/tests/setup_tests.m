a3ea95f7d82c7c4709fe86435299f6b35b9c0267
jcockfie <jeremy.cockfield@gmail.com>
2018-08-06 16:50:42 -0500

    changed setup_tests to use integrated test framework

--- matlab/tests/setup_tests.m
+++ matlab/tests/setup_tests.m
@@ -7,17 +7,17 @@
 % sample EEGLAB study which can be found at:
 %    ftp://sccn.ucsd.edu/pub/5subjects_full.zip
 %
-values.testroot = 'Path\To\HEDToolsTestArchive';
-values.studydir = '5subjects';
-values.studyname = 'n400clustedit.study';
-values.shooterdir = 'ShooterSet';
-values.BCI2000dir = 'BCI2000Set';
-values.Otherdir = 'Other';
-values.EEGLAB = 'EEGLABSet';
-values.efile1 = 'BCIT1.csv';
-values.efile2 = 'BCIT2.csv';
-values.efile3 = 'BCIT3.csv';
-values.emptyfile = 'BCITempty.csv';
-values.onerow = 'BCIT1Row.csv';
-values.badfile = 'badfile';
-values.dbfile = 'dbcreds.txt';
+testCase.TestData.testroot = 'Path\To\HEDToolsTestArchive';
+testCase.TestData.studydir = '5subjects';
+testCase.TestData.studyname = 'n400clustedit.study';
+testCase.TestData.shooterdir = 'ShooterSet';
+testCase.TestData.BCI2000dir = 'BCI2000Set';
+testCase.TestData.Otherdir = 'Other';
+testCase.TestData.EEGLAB = 'EEGLABSet';
+testCase.TestData.efile1 = 'BCIT1.csv';
+testCase.TestData.efile2 = 'BCIT2.csv';
+testCase.TestData.efile3 = 'BCIT3.csv';
+testCase.TestData.emptyfile = 'BCITempty.csv';
+testCase.TestData.onerow = 'BCIT1Row.csv';
+testCase.TestData.badfile = 'badfile';
+testCase.TestData.dbfile = 'dbcreds.txt';
