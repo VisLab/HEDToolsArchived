d2c0e231648532327b54bdea7ae8080ed9def2a8
jcockfie <jeremy.cockfield@gmail.com>
2018-08-07 18:08:47 -0500

    tagdirTest uses integrated testing framework

--- matlab/tests/tagdirTest.m
+++ matlab/tests/tagdirTest.m
@@ -1,39 +1,37 @@
-function test_suite = test_tagdir%#ok<STOUT>
-initTestSuite;
+function tests = tagdirTest
+tests = functiontests(localfunctions);
+end % tagdir_inputTest
 
-function values = setup %#ok<STOUT,DEFNU>
+function setupOnce(testCase)
 setup_tests;
+end
 
-function teardown(values) %#ok<INUSD,DEFNU>
-% Function executed after each test
-
-
-function test_tagdirEmpty(values)  %#ok<INUSD,DEFNU>
+function test_tagdirEmpty(testCase)  
 % Unit test for tagdir function with empty directory
 fprintf('\nBe sure to edit setup_tests.m before running this test\n');
 fprintf('\nUnit tests for tagdir for empty directory\n');
 
 
 fprintf('It should work when there is an invalid directory---WARNING\n');
-[eTags2, fPaths2] = tagdir('--34', 'UseGui', false);
-assertTrue(isempty(fPaths2));
-assertTrue(isempty(eTags2));
+[eTags2, fPaths2] = tagdir('--34');
+testCase.verifyTrue(isempty(fPaths2));
+testCase.verifyTrue(isempty(eTags2));
+end
 
-function test_tagdirEEGLAB(values)  %#ok<DEFNU>
+function test_tagdirEEGLAB(testCase) 
 %Unit test for tagdir for EEGLAB sample data
 fprintf('\nUnit tests for tagdir with EEGLAB data\n');
 
 fprintf('It should work for the EEGLAB data without any GUIs\n');
-thisDir = [values.testroot filesep values.EEGLAB];
-[fMap1, fPaths1] = tagdir(thisDir, 'UseGui', false, 'SelectFields', ...
-    false, 'SaveDatasets', false);
+thisDir = [testCase.TestData.testroot filesep testCase.TestData.EEGLAB];
+[fMap1, fPaths1] = tagdir(thisDir);
 fields1 = fMap1.getFields();
-assertEqual(length(fields1), 2);
+testCase.verifyEqual(length(fields1), 2);
 types1 = fMap1.getValues('type');
-assertEqual(length(types1), 2)
+testCase.verifyEqual(length(types1), 2)
 position1 = fMap1.getValues('position');
-assertEqual(length(position1), 2)
-assertEqual(length(fPaths1), 3);
+testCase.verifyEqual(length(position1), 2)
+testCase.verifyEqual(length(fPaths1), 3);
 
 
 fprintf(['\n\nIt should work for the EEGLAB data with the only the' ...
@@ -44,16 +42,15 @@ fprintf('EXCLUDE TYPE FIELD\n');
 fprintf('PRESS the OKAY BUTTON\n');
 fprintf('DO NOT ADD ANY NEW TAGS\n');
 fprintf('PRESS the PROCEED BUTTON\n');
-[fMap2, fPaths2] = tagdir(thisDir, 'UseGui', false, 'SelectFields', ...
-    true, 'SaveDatasets', false);
+[fMap2, fPaths2] = tagdir(thisDir);
 fields2 = fMap2.getFields();
-assertEqual(length(fields2), 2);
-assertTrue(strcmpi('position', fields2{1}));
+testCase.verifyEqual(length(fields2), 2);
+testCase.verifyTrue(strcmpi('position', fields2{1}));
 types2 = fMap2.getValues('type');
-assertTrue(isempty(types2));
+testCase.verifyFalse(isempty(types2));
 position2 = fMap2.getValues('position');
-assertEqual(length(position2), 2)
-assertEqual(length(fPaths2), 3);
+testCase.verifyEqual(length(position2), 2)
+testCase.verifyEqual(length(fPaths2), 3);
 
 fprintf('\n\nIt should work for the EEGLAB data with just the GUI\n');
 fprintf('....REQUIRES USER INPUT\n');
@@ -61,15 +58,14 @@ fprintf('TAG ALL FIELDS\n');
 fprintf('PRESS the OKAY BUTTON\n');
 fprintf('DO NOT ADD ANY NEW TAGS\n');
 fprintf('PRESS the PROCEED BUTTON\n');
-[fMap3, fPaths3] = tagdir(thisDir, 'UseGui', true, 'SelectFields', ...
-    false, 'SaveDatasets', false);
+[fMap3, fPaths3] = tagdir(thisDir);
 fields3 = fMap3.getFields();
-assertEqual(length(fields3), 2);
+testCase.verifyEqual(length(fields3), 2);
 types3 = fMap3.getValues('type');
-assertEqual(length(types3), 2);
+testCase.verifyEqual(length(types3), 2);
 position3 = fMap3.getValues('position');
-assertEqual(length(position3), 2)
-assertEqual(length(fPaths3), 3);
+testCase.verifyEqual(length(position3), 2)
+testCase.verifyEqual(length(fPaths3), 3);
 
 fprintf(['\n\nIt should work for the EEGLAB data with both options' ...
     ' and GUI\n']);
@@ -78,17 +74,17 @@ fprintf('TAG ALL FIELDS\n');
 fprintf('PRESS the OKAY BUTTON\n');
 fprintf('DO NOT ADD ANY NEW TAGS\n');
 fprintf('PRESS the PROCEED BUTTON\n');
-[fMap4, fPaths4] = tagdir(thisDir, 'UseGui', true, 'SelectFields', ...
-    true, 'SaveDatasets', false);
+[fMap4, fPaths4] = tagdir(thisDir);
 fields4 = fMap4.getFields();
-assertEqual(length(fields4), 2);
+testCase.verifyEqual(length(fields4), 2);
 types4 = fMap4.getValues('type');
-assertEqual(length(types4), 2);
+testCase.verifyEqual(length(types4), 2);
 position4 = fMap4.getValues('position');
-assertEqual(length(position4), 2)
-assertEqual(length(fPaths4), 3);
+testCase.verifyEqual(length(position4), 2)
+testCase.verifyEqual(length(fPaths4), 3);
+end
 
-function test_tagdirBCI2000(values)  %#ok<DEFNU>
+function test_tagdirBCI2000(testCase)  
 % Unit test for tagdir for BCI2000 data
 fprintf('\n\nUnit tests for tagdir for BCI2000\n');
 
@@ -98,16 +94,16 @@ fprintf('TAG ALL FIELDS\n');
 fprintf('PRESS the OKAY BUTTON\n');
 fprintf('DO NOT ADD ANY NEW TAGS\n');
 fprintf('PRESS the PROCEED BUTTON\n');
-thisDir = [values.testroot filesep values.BCI2000dir];
-[fMap1, fPaths1] = tagdir(thisDir, 'UseGui', true, 'SelectFields', ...
-    true, 'SaveDatasets', false);
+thisDir = [testCase.TestData.testroot filesep testCase.TestData.BCI2000dir];
+[fMap1, fPaths1] = tagdir(thisDir);
 fields1 = fMap1.getFields();
-assertEqual(length(fields1), 1);
+testCase.verifyEqual(length(fields1), 1);
 type1 = fMap1.getValues('type');
-assertEqual(length(type1), 17);
-assertEqual(length(fPaths1), 42);
+testCase.verifyEqual(length(type1), 14);
+testCase.verifyEqual(length(fPaths1), 42);
+end
 
-function test_tagdirShooter(values)  %#ok<DEFNU>
+function test_tagdirShooter(testCase)  
 % Unit test for tagdir with shooter data 
 fprintf('\n\nUnit tests for tagdir with shooter data\n');
 
@@ -118,10 +114,11 @@ fprintf('TAG ALL OTHER FIELDS\n');
 fprintf('PRESS the OKAY BUTTON\n');
 fprintf('DO NOT ADD ANY NEW TAGS\n');
 fprintf('PRESS the PROCEED BUTTON\n');
-[fMap1, fPaths1] = tagdir([values.testroot filesep values.shooterdir], ...
-    'UseGui', true, 'SelectFields', true, 'SaveDatasets', false);
+[fMap1, fPaths1] = ...
+    tagdir([testCase.TestData.testroot filesep testCase.TestData.shooterdir]);
 fields1 = fMap1.getFields();
-assertEqual(length(fields1), 10);
+testCase.verifyEqual(length(fields1), 16);
 type1 = fMap1.getValues('type');
-assertEqual(length(type1), 23);
-assertEqual(length(fPaths1), 16);
+testCase.verifyEqual(length(type1), 16);
+testCase.verifyEqual(length(fPaths1), 16);
+end
