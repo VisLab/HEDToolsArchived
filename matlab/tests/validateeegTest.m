3e39c054a2f1b3632322051d4e6a6361f498e588
jcockfie <jeremy.cockfield@gmail.com>
2018-08-07 16:28:17 -0500

    validateeegTest uses integrated testing framework

--- matlab/tests/validateeegTest.m
+++ matlab/tests/validateeegTest.m
@@ -1,46 +1,46 @@
-function test_suite = test_validateeeg%#ok<STOUT>
-initTestSuite;
+function tests = validateeegTest
+tests = functiontests(localfunctions);
+end % validateeegTest
 
-function values = setup %#ok<DEFNU>
-values.EEG1.event(1).usertags = '';
-values.EEG2.event(1).usertags = ['Event/Label/Test1,' ...
+function setupOnce(testCase)
+testCase.TestData.EEG1.event(1).usertags = '';
+testCase.TestData.EEG2.event(1).usertags = ['Event/Label/Test1,' ...
     'Event/Category/Participant response,' ...
     'Event/Description/This is a test,' ...
     '(Participant ~ ' ...
     'Action/Control vehicle/Drive/Collide ~ ' ...
     'Item/Object/Vehicle/Car)'];
-values.EEG3.event(1).usertags = ['Event/Label/Test1,' ...
+testCase.TestData.EEG3.event(1).usertags = ['Event/Label/Test1,' ...
     'Event/Category/Participant response,' ...
     'Event/Description/This is a test (Participant ~ ' ...
     'Action/Control vehicle/Drive/Collide ~ ' ...
     'Item/Object/Vehicle/Car)'];
-values.EEG4.event(1).usertags = ['(Participant ~ ' ...
+testCase.TestData.EEG4.event(1).usertags = ['(Participant ~ ' ...
     'Action/Control vehicle/Drive/Collide ~ ' ...
     'Item/Object/Vehicle/Car),' ...
     'Event/Label/Test1,' ...
     'Event/Category/Participant response,'];
+end
 
-function teardown(values) %#ok<INUSD,DEFNU>
-% Function executed after each test
-
-function testValidateEEG(values)  %#ok<DEFNU>
+function testValidateEEG(testCase)  
 % Unit test for editmaps
 fprintf('\nUnit tests for validateeeg\n');
 
 fprintf('\nIt should return no errors when there is a empty HED string');
-errors = validateeeg(values.EEG1);
-assertTrue(isempty(errors));
+errors = validateeeg(testCase.TestData.EEG1);
+testCase.verifyTrue(isempty(errors));
 
 fprintf('\nIt should return no errors when the HED string is valid');
-errors = validateeeg(values.EEG2);
-assertTrue(isempty(errors));
+errors = validateeeg(testCase.TestData.EEG2);
+testCase.verifyTrue(isempty(errors));
 
 fprintf(['\nIt should return errors when there is a missing comma' ...
     ' before a group']);
-errors = validateeeg(values.EEG3);
-assertFalse(isempty(errors));
+errors = validateeeg(testCase.TestData.EEG3);
+testCase.verifyFalse(isempty(errors));
 
 fprintf(['\nIt should return errors when there is a required tag' ...
     ' missing']);
-errors = validateeeg(values.EEG4);
-assertFalse(isempty(errors));
+errors = validateeeg(testCase.TestData.EEG4);
+testCase.verifyFalse(isempty(errors));
+end
