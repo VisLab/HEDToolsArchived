b325a11e453b900852b2e6f06bcec4cfeedb8797
jcockfie <jeremy.cockfield@gmail.com>
2018-08-13 15:45:20 -0500

    editmapsTest uses integrated testing framework

--- matlab/tests/editmapsTest.m
+++ matlab/tests/editmapsTest.m
@@ -1,10 +1,10 @@
-function test_suite = test_editmaps%#ok<STOUT>
-initTestSuite;
+function tests = editmapsTest
+tests = functiontests(localfunctions);
+end % editmapsTest
 
-function values = setup %#ok<DEFNU>
-% Read in the HED schema
+function setupOnce(testCase)
 latestHed = 'HED.xml';
-values.xml = fileread(latestHed);
+testCase.TestData.xml = fileread(latestHed);
 s1(1) = tagList('square');
 s1(1).add({'/Attribute/Visual/Color/Green', ...
     '/Item/2D shape/Rectangle/Square'});
@@ -14,29 +14,26 @@ s2(1) = tagList('1');
 s2(1).add('/Attribute/Object orientation/Rotated/Degrees/3 degrees');
 s2(2) = tagList('2');
 s2(2).add('/Attribute/Object orientation/Rotated/Degrees/1.5 degrees');
-values.map1 = fieldMap('XML', values.xml);
-values.map1.addValues('type', s1);
-values.map2 = fieldMap('XML', values.xml);
-values.map2.addValues('position', s2);
+testCase.TestData.map1 = fieldMap('XML', testCase.TestData.xml);
+testCase.TestData.map1.addValues('type', s1);
+testCase.TestData.map2 = fieldMap('XML', testCase.TestData.xml);
+testCase.TestData.map2.addValues('position', s2);
+end
 
-function teardown(values) %#ok<INUSD,DEFNU>
-% Function executed after each test
-
-
-function testSubmit(values)  %#ok<DEFNU>
+function testSubmit(testCase)
 % Unit test for editmaps
 fprintf('\nUnit tests for editmaps normal execution\n');
 fprintf('\nIt should not modify the number of tags\n')
 fprintf('....REQUIRES USER INPUT\n');
 fprintf('DO NOT ADD ANY NEW TAGS\n');
-fprintf('PRESS PROCEED BUTTON\n');
-fMap = values.map1;
+fprintf('PRESS DONE BUTTON\n');
+fMap = testCase.TestData.map1;
 editmaps(fMap.clone());
 
 fprintf('\nIt should modify the number of tags\n');
 fprintf('....REQUIRES USER INPUT\n');
 fprintf('ADD 2 NEW TAGS TO THE FIRST EVENT\n');
-fprintf('PRESS PROCEED BUTTON\n');
+fprintf('PRESS DONE BUTTON\n');
 fMap2 = editmaps(fMap.clone());
 events1 = fMap.getValues('type');
 count1 = 0;
@@ -49,7 +46,7 @@ for k = 1:length(events1)
     end
 end
 events2 = fMap2.getValues('type');
-assertEqual(length(events2), 2);
+testCase.verifyEqual(length(events2), 2);
 
 fprintf('\nIt should increase the number of tags by 2\n');
 count2 = 0;
@@ -61,11 +58,11 @@ for k = 1:length(events2)
         count2 = count2 + length(etags);
     end
 end
-assertEqual(count2, count1 + 2);
+testCase.verifyEqual(count2, count1 + 2);
 fprintf('\nIt should modify the number of tags\n');
 fprintf('....REQUIRES USER INPUT\n');
 fprintf('ADD 3 NEW TAGS TO THE SECOND EVENT\n');
-fprintf('PRESS PROCEED BUTTON\n');
+fprintf('PRESS DONE BUTTON\n');
 fMap3 = editmaps(fMap2);
 events3 = fMap3.getValues('type');
 fprintf('\nIt should increase the number of tags by 3\n');
@@ -78,24 +75,27 @@ for k = 1:length(events3)
         count3 = count3 + length(etags);
     end
 end
-assertEqual(count3, count2 + 3);
+testCase.verifyEqual(count3, count2 + 3);
+end
 
-function testMultipleFields(values)  %#ok<DEFNU>
+function testMultipleFields(testCase)  
 % Unit test for editmaps
 fprintf('\nUnit tests for editmaps multiple fields\n');
-fprintf('\nIt should work when the proceed button is pressed\n');
+fprintf('\nIt should work when the DONE button is pressed\n');
 fprintf('....REQUIRES USER INPUT\n');
 fprintf('DO NOT ADD ANY NEW TAGS\n');
-fprintf('PRESS PROCEED BUTTON\n');
-fMap1 = editmaps(values.map2);
-assertEqual(fMap1, values.map2);
+fprintf('PRESS DONE BUTTON\n');
+fMap1 = editmaps(testCase.TestData.map2);
+testCase.verifyEqual(fMap1, testCase.TestData.map2);
+end
 
-function testCancel(values)  %#ok<DEFNU>
+function testCancel(testCase)  
 % Unit test for editmaps
-fprintf('\nUnit tests for editmaps cancel\n');
-fprintf('\nIt should work when the cancel button is pressed\n');
+fprintf('\nUnit tests for editmaps exit\n');
+fprintf('\nIt should work when the exit menu option is selected\n');
 fprintf('....REQUIRES USER INPUT\n');
 fprintf('DO NOT ADD ANY NEW TAGS\n');
-fprintf('PRESS CANCEL BUTTON\n');
-fMap1 = editmaps(values.map2);
-assertEqual(fMap1, values.map2);
+fprintf('SELECT EXIT OPTION UNDER FILE MENU\n');
+fMap1 = editmaps(testCase.TestData.map2);
+testCase.verifyEqual(fMap1, testCase.TestData.map2);
+end
