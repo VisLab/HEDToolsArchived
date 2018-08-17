d6eeb5c0f9f6823e032f4ba439c38ad9da8ba8b9
jcockfie <jeremy.cockfield@gmail.com>
2018-08-17 16:20:38 -0500

    changed valueLim to valuelim

--- matlab/helpers/interface/epochhed_input.m
+++ matlab/helpers/interface/epochhed_input.m
@@ -95,7 +95,7 @@
 %                Epoch latency limits [start end] in seconds relative to
 %                the time-locking event {default: [-1 2]}
 %
-%   valueLim
+%   valuelim
 %                Lower and upper bound latencies for trial data. Else if
 %                one positive value is given, use its negative as the lower
 %                bound. The given values are also considered outliers
